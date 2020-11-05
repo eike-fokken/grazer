bf61cfdce3981a011a61b1bf3c4ad7376598d91c
Eike <e.fokken+git@posteo.de>
2020-11-05 14:11:18 +0100

    changed boundary value to operator

--- src/Model/Networkproblem/include/Boundaryvalue.hpp
+++ src/Model/Networkproblem/include/Boundaryvalue.hpp
@@ -3,12 +3,13 @@
 #include <vector>
 
 namespace Model::Networkproblem {
-class Boundelement {
+class Boundaryvalue {
 
-  Boundelement(std::map<double, Eigen::VectorXd> _boundary_values)
+  Boundaryvalue(std::map<double, Eigen::VectorXd> _boundary_values)
       : boundary_values(_boundary_values){};
 
-  Eigen::VectorXd get_boundaryvalue(double t);
+  Eigen::VectorXd operator()(double t) const;
+  Eigen::VectorXd operator()(double t);
 
 private:
   std::map<double, Eigen::VectorXd> boundary_values;
