e65a788a73eb590db026520510bdf1a4ca8d1c5c
Eike <e.fokken+git@posteo.de>
2020-11-06 17:53:15 +0100

    work on power

--- src/Model/Power/include/Vphinode.hpp
+++ src/Model/Power/include/Vphinode.hpp
@@ -11,11 +11,11 @@ public:
   /// In this node we just set V and phi to their respective boundary values.
   virtual void evaluate(Eigen::VectorXd &rootfunction, double last_time,
                         double new_time, Eigen::VectorXd const &last_state,
-                        Eigen::VectorXd const &new_state) override;
+                        Eigen::VectorXd const &new_state) override final;
 
   virtual void evaluate_state_derivative(
       Eigen::SparseMatrix<double> &jacobian, double last_time, double new_time,
-      Eigen::VectorXd const &, Eigen::VectorXd const &new_state) override;
+      Eigen::VectorXd const &, Eigen::VectorXd const &new_state) override final;
 
 private:
   Boundaryvalue<Vphinode, 2> boundaryvalue;
