3602479c14c33afb265b034b87edd6b7aa5259b1
Eike <e.fokken+git@posteo.de>
2020-11-11 23:05:31 +0100

    matrixhandler wip

--- src/Aux/Matrixhandler/Matrixhandler.cpp
+++ src/Aux/Matrixhandler/Matrixhandler.cpp
@@ -1,3 +1,19 @@
+#include <Eigen/Sparse>
 #include <Matrixhandler.hpp>
 
-  
+namespace Aux {
+
+  void Triplethandler::set_coefficient(int row, int col, double value) {
+    Eigen::Triplet<double> newtriplet(row, col, value);
+    tripletlist.push_back(newtriplet);
+  }
+
+  void Triplethandler::set_matrix() {
+    matrix->setFromTriplets(tripletlist.begin(), tripletlist.end());
+  }
+
+  void Coeffrefhandler::set_coefficient(int row, int col, double value) {
+    matrix->coeffRef(row, col) = value;
+  }
+
+} // namespace Aux
--- src/Aux/Matrixhandler/include/Matrixhandler.hpp
+++ src/Aux/Matrixhandler/include/Matrixhandler.hpp
@@ -1,58 +1,45 @@
+#pragma once
 #include <Eigen/Sparse>
 #include <vector>
 
 namespace Aux {
 
-class Matrixhandler {
-
-public:
-
-  Matrixhandler (Eigen::SparseMatrix<double> * _matrix): matrix(_matrix) {};
-
-  virtual ~Matrixhandler () {};
-
-  virtual void set_coefficient(int row, int col, double value);
-
-  virtual void set_matrix();
-
-
-protected:
-
-  Eigen::SparseMatrix<double> * matrix;
-
-};
-
-
-  class Triplethandler: public Matrixhandler {
+  class Matrixhandler {
 
   public:
+    Matrixhandler(Eigen::SparseMatrix<double> *_matrix) : matrix(_matrix){};
 
-    virtual ~Triplethandler () override {};
+    virtual ~Matrixhandler(){};
+
+    virtual void set_coefficient(int row, int col, double value) = 0;
+
+    virtual void set_matrix() = 0;
+
+  protected:
+    Eigen::SparseMatrix<double> *matrix;
+  };
+
+  class Triplethandler final : public Matrixhandler {
+
+  public:
+    virtual ~Triplethandler() override{};
 
     virtual void set_coefficient(int row, int col, double value) override final;
 
     virtual void set_matrix() override final;
-
 
   private:
-
-    std::vector<Eigen::Triplet<double>>  tripletlist;
-
+    std::vector<Eigen::Triplet<double>> tripletlist;
   };
 
-  class Coeffrefhandler: public Matrixhandler {
+  class Coeffrefhandler final : public Matrixhandler {
 
   public:
-
-    virtual ~Coeffrefhandler () override {};
+    virtual ~Coeffrefhandler() override{};
 
     virtual void set_coefficient(int row, int col, double value) override final;
 
     virtual void set_matrix() override final;
-
   };
 
-
-
-
-}
+} // namespace Aux
