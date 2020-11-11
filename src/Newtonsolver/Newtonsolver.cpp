493702d088d16669991292664d612f3cb0ad631b
Eike <e.fokken+git@posteo.de>
2020-11-12 00:42:20 +0100

    now there is a solve function

--- src/Newtonsolver/Newtonsolver.cpp
+++ src/Newtonsolver/Newtonsolver.cpp
@@ -1,25 +1,108 @@
+#include <Eigen/Sparse>
 #include <Matrixhandler.hpp>
 #include <Newtonsolver.hpp>
 #include <Problem.hpp>
 
 namespace Solver {
 
-  void Newtonsolver::update_jabobian_triplets(
+  void Newtonsolver::evaluate_state_derivative_triplets(
       Model::Problem &problem, double last_time, double new_time,
       Eigen::VectorXd const &last_state, Eigen::VectorXd const &new_state) {
-    Aux::Triplethandler handle(&jacobian);
-    Aux::Triplethandler *const handle_ptr(&handle);
-    problem.evaluate_state_derivative(handle_ptr, last_time, new_time,
-                                      last_state, new_state);
-    handle.set_matrix();
+    {
+      jacobian.resize(new_state.size(), new_state.size());
+      Aux::Triplethandler handler(&jacobian);
+      Aux::Triplethandler *const handler_ptr = &handler;
+      problem.evaluate_state_derivative(handler_ptr, last_time, new_time,
+                                        last_state, new_state);
+      handler.set_matrix();
+    }
+    sparselusolver.analyzePattern(jacobian);
   }
-  void Newtonsolver::update_jabobian_coeffref(
-      Model::Problem &problem, double last_time, double new_time,
-      Eigen::VectorXd const &last_state, Eigen::VectorXd const &new_state) {
 
-    Aux::Coeffrefhandler handle(&jacobian);
-    Aux::Coeffrefhandler *const handle_ptr(&handle);
-    problem.evaluate_state_derivative(handle_ptr, last_time, new_time,
+  void Newtonsolver::evaluate_state_derivative_coeffref(
+      Model::Problem &problem, double last_time, double new_time,
+      Eigen::VectorXd const &last_state, Eigen::VectorXd const &new_state) {
+    Aux::Coeffrefhandler handler(&jacobian);
+    Aux::Coeffrefhandler *const handler_ptr = &handler;
+    problem.evaluate_state_derivative(handler_ptr, last_time, new_time,
                                       last_state, new_state);
   }
+
+  Solutionstruct Newtonsolver::solve(Eigen::VectorXd &new_state,
+                                     Model::Problem &problem, double last_time,
+                                     double new_time,
+                                     Eigen::VectorXd const &last_state) {
+    Solutionstruct solstruct;
+    // If the structure of the jacobian changed one should generate it from
+    // triplets anew:
+    if (last_state.size() != new_state.size()) {
+      evaluate_state_derivative_triplets(problem, last_time, new_time,
+                                         last_state, new_state);
+    }
+    // If the non-zero elements are the same, just update them:
+    else {
+      evaluate_state_derivative_coeffref(problem, last_time, new_time,
+                                         last_state, new_state);
+    }
+
+    Eigen::VectorXd temp1(new_state.size());
+    Eigen::VectorXd temp2(new_state.size());
+    Eigen::VectorXd *vec1 = &temp1;
+    // They are not created with the new keyword,
+    Eigen::VectorXd *vec2 = &temp2;
+    // and instead reference the two temp objects, because Eigen
+    // doesn't play nice with delete.
+    Eigen::VectorXd *temp;
+
+    Eigen::VectorXd function(new_state.size());
+    Eigen::VectorXd step(new_state.size());
+    double stepsizeparameter;
+    double oldstepsizeparameter;
+    double stepsize;
+
+    *vec1 = new_state;
+    problem.evaluate(function, last_time, new_time, last_state, *vec1);
+    solstruct.residual = function.lpNorm<Eigen::Infinity>();
+
+    while (solstruct.residual > tolerance &&
+           solstruct.used_iterations < maximal_iterations) {
+      // compute new derivative before the next step:
+      problem.evaluate(function, last_time, new_time, last_state, *vec2);
+      evaluate_state_derivative_coeffref(problem, last_time, new_time,
+                                         last_state, *vec1);
+      sparselusolver.factorize(jacobian);
+      step = sparselusolver.solve(-function);
+      oldstepsizeparameter = step.lpNorm<2>();
+      stepsize = 2.0; // This is set to 2.0, so that in the first run of the
+                      // following loop the stepsize is decreased to the correct
+                      // initial value of 1.0.
+
+      // Stepsize computation:
+      do {
+        stepsize *= 0.5;
+        *vec2 = *vec1 + stepsize * step;
+        problem.evaluate(function, last_time, new_time, last_state, *vec2);
+        stepsizeparameter = sparselusolver.solve(-function).lpNorm<2>();
+      } while ((stepsizeparameter >
+                (1 - decrease_value * stepsize) * oldstepsizeparameter) &&
+               stepsize > minimal_stepsize);
+
+      solstruct.residual = function.lpNorm<Eigen::Infinity>();
+      temp = vec1;
+      vec1 = vec2; // we switch pointers in order to not copy data and still
+                   // have access to the last value.
+      vec2 = temp;
+      ++solstruct.used_iterations;
+    }
+    new_state = *vec1;
+
+    if (solstruct.used_iterations == maximal_iterations) {
+      solstruct.success = false;
+    } else {
+      solstruct.success = true;
+    }
+
+    return solstruct;
+  }
+
 } // namespace Solver
--- src/Newtonsolver/include/Newtonsolver.hpp
+++ src/Newtonsolver/include/Newtonsolver.hpp
@@ -1,9 +1,6 @@
+#pragma once
 #include <Eigen/Sparse>
-// #include <eigen3/Eigen/src/SparseCore/SparseMatrix.h>
-
-namespace Aux {
-  class Matrixhandler;
-}
+#include <Eigen/SparseLU>
 
 namespace Model {
   class Problem;
@@ -11,33 +8,53 @@ namespace Model {
 
 namespace Solver {
 
-  struct Solutionobject {
+  struct Solutionstruct {
     bool success{false};
     double residual{std::numeric_limits<double>::quiet_NaN()};
     int used_iterations{0};
   };
 
+  /// This class holds a SparseMatrix and a corresponding Sparse-matrix solver,
+  /// so it can compute the solution of a non-linear problem.
   class Newtonsolver {
   public:
-    void update_jabobian_triplets(Model::Problem &problem, double last_time,
-                                  double new_time,
-                                  Eigen::VectorXd const &last_state,
-                                  Eigen::VectorXd const &new_state);
+    Newtonsolver(double _tolerance, int _maximal_iterations)
+        : tolerance(_tolerance), maximal_iterations(_maximal_iterations){};
 
-    void update_jabobian_coeffref(Model::Problem &problem, double last_time,
-                                  double new_time,
-                                  Eigen::VectorXd const &last_state,
-                                  Eigen::VectorXd const &new_state);
+    void evaluate_state_derivative_triplets(Model::Problem &problem,
+                                            double last_time, double new_time,
+                                            Eigen::VectorXd const &last_state,
+                                            Eigen::VectorXd const &new_state);
 
-    Solutionobject solve();
+    void evaluate_state_derivative_coeffref(Model::Problem &problem,
+                                            double last_time, double new_time,
+                                            Eigen::VectorXd const &last_state,
+                                            Eigen::VectorXd const &new_state);
+
+    Solutionstruct solve(Eigen::VectorXd &new_state, Model::Problem &problem,
+                         double last_time, double new_time,
+                         Eigen::VectorXd const &last_state);
 
   private:
-    constexpr static double const decrease_value{0.01};
-    constexpr static double const minimal_stepsize{1e-10};
-
+    /// Holds an instance of the actual solver, to save computation time it is
+    /// kept from previous time steps because usually the sparsity pattern
+    /// will not change.
     Eigen::SparseLU<Eigen::SparseMatrix<double>, Eigen::COLAMDOrdering<int>>
         sparselusolver;
+    /// This must be revisited when including on the fly refinement of meshes.
+
+    /// This will be the jacobian matrix.  We hold it here so its sparsity
+    /// pattern is preserved.
     Eigen::SparseMatrix<double> jacobian;
+
+    /// Tolerance under which equality is accepted.
+    double tolerance;
+
+    /// highest number of iterations after which to throw an exception
+    int maximal_iterations;
+
+    constexpr static double const decrease_value{0.01};
+    constexpr static double const minimal_stepsize{1e-10};
   };
 
 } // namespace Solver
