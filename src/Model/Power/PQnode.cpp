4640068e7dfe490dcf3c2e4a021e3e9b5d1453d0
Eike <e.fokken+git@posteo.de>
2020-11-17 16:50:31 +0100

    refactored P, Q and their derivatives into Powernode.
    This should be a little slower, but much cleaner.

--- src/Model/Power/PQnode.cpp
+++ src/Model/Power/PQnode.cpp
@@ -11,45 +11,8 @@ namespace Model::Networkproblem::Power {
                         Eigen::VectorXd const &new_state) {
     int index1 = get_start_state_index();
     int index2 = index1 + 1;
-    double G_i = get_G();
-    double B_i = get_B();
-    double V_i = new_state[index1];
-    double phi_i = new_state[index2];
-
-    double P = -boundaryvalue(new_time)[0];
-    double Q = -boundaryvalue(new_time)[1];
-    P += G_i * V_i * V_i;
-    Q -= B_i * V_i * V_i;
-    for (auto &start_edge : get_starting_edges()) {
-      auto line = dynamic_cast<Transmissionline const *>(start_edge);
-      double G_ik = line->get_G();
-      double B_ik = line->get_B();
-      auto othernode = dynamic_cast<Powernode *>(start_edge->get_ending_node());
-      int index1_k = othernode->get_start_state_index();
-      int index2_k = index1_k + 1;
-      double V_k = new_state[index1_k];
-      double phi_k = new_state[index2_k];
-      double phi_ik = phi_i - phi_k;
-      P += V_i * V_k * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik));
-      Q += V_i * V_k * (G_ik * sin(phi_ik) - B_ik * cos(phi_ik));
-    }
-
-    for (auto &end_edge : get_ending_edges()) {
-      auto line = dynamic_cast<Transmissionline const *>(end_edge);
-      double G_ik = line->get_G();
-      double B_ik = line->get_B();
-      Powernode *othernode =
-          dynamic_cast<Powernode *>(end_edge->get_starting_node());
-      int index1_k = othernode->get_start_state_index();
-      int index2_k = index1_k + 1;
-      double V_k = new_state[index1_k];
-      double phi_k = new_state[index2_k];
-      double phi_ik = phi_i - phi_k;
-      P += V_i * V_k * (G_ik * cos(phi_ik) - B_ik * sin(phi_ik));
-      Q += V_i * V_k * (G_ik * sin(phi_ik) - B_ik * cos(phi_ik));
-    }
-    rootfunction[index1] = P;
-    rootfunction[index2] = Q;
+    rootfunction[index1] = P(new_time, new_state);
+    rootfunction[index2] = Q(new_time, new_state);
   }
 
   void PQnode::evaluate_state_derivative(Aux::Matrixhandler *jacobianhandler,
@@ -59,86 +22,8 @@ namespace Model::Networkproblem::Power {
                                          ,
                                          Eigen::VectorXd const &,
                                          Eigen::VectorXd const &new_state) {
-    int index1 = get_start_state_index();
-    int index2 = index1 + 1;
-    double G_i = get_G();
-    double B_i = get_B();
-    double V_i = new_state[index1];
-    double phi_i = new_state[index2];
-
-    jacobianhandler->set_coefficient(index1, index1, 2 * G_i * V_i);
-    jacobianhandler->set_coefficient(index1, index2, 0.0);
-
-    jacobianhandler->set_coefficient(index2, index1, -2 * B_i * V_i);
-    jacobianhandler->set_coefficient(index2, index2, 0.0);
-
-    for (auto &start_edge : get_starting_edges()) {
-      auto line = dynamic_cast<Transmissionline const *>(start_edge);
-      double G_ik = line->get_G();
-      double B_ik = line->get_B();
-      auto othernode = dynamic_cast<Powernode *>(start_edge->get_ending_node());
-      int index1_k = othernode->get_start_state_index();
-      int index2_k = index1_k + 1;
-      double V_k = new_state[index1_k];
-      double phi_k = new_state[index2_k];
-      double phi_ik = phi_i - phi_k;
-
-      jacobianhandler->add_to_coefficient(
-          index1, index1, V_k * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik)));
-      jacobianhandler->add_to_coefficient(
-          index1, index2,
-          V_i * V_k * (-G_ik * sin(phi_ik) + B_ik * cos(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index1, index1_k, V_i * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index1, index2_k,
-          V_i * V_k * (G_ik * sin(phi_ik) + B_ik * cos(phi_ik)));
-
-      jacobianhandler->add_to_coefficient(
-          index2, index1, V_k * (G_ik * sin(phi_ik) - B_ik * cos(phi_ik)));
-      jacobianhandler->add_to_coefficient(
-          index2, index2,
-          V_i * V_k * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index2, index1_k, V_i * (G_ik * sin(phi_ik) - B_ik * cos(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index2, index2_k,
-          V_i * V_k * (-G_ik * cos(phi_ik) - B_ik * sin(phi_ik)));
-    }
-
-    for (auto &end_edge : get_ending_edges()) {
-      auto line = dynamic_cast<Transmissionline const *>(end_edge);
-      double G_ik = line->get_G();
-      double B_ik = line->get_B();
-      Powernode *othernode =
-          dynamic_cast<Powernode *>(end_edge->get_starting_node());
-      int index1_k = othernode->get_start_state_index();
-      int index2_k = index1_k + 1;
-      double V_k = new_state[index1_k];
-      double phi_k = new_state[index2_k];
-      double phi_ik = phi_i - phi_k;
-      jacobianhandler->add_to_coefficient(
-          index1, index1, V_k * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik)));
-      jacobianhandler->add_to_coefficient(
-          index1, index2,
-          V_i * V_k * (-G_ik * sin(phi_ik) + B_ik * cos(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index1, index1_k, V_i * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index1, index2_k,
-          V_i * V_k * (G_ik * sin(phi_ik) + B_ik * cos(phi_ik)));
-
-      jacobianhandler->add_to_coefficient(
-          index2, index1, V_k * (G_ik * sin(phi_ik) - B_ik * cos(phi_ik)));
-      jacobianhandler->add_to_coefficient(
-          index2, index2,
-          V_i * V_k * (G_ik * cos(phi_ik) + B_ik * sin(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index2, index1_k, V_i * (G_ik * sin(phi_ik) - B_ik * cos(phi_ik)));
-      jacobianhandler->set_coefficient(
-          index2, index2_k,
-          V_i * V_k * (-G_ik * cos(phi_ik) - B_ik * sin(phi_ik)));
-    }
+    evaluate_P_derivative(jacobianhandler, new_state);
+    evaluate_Q_derivative(jacobianhandler, new_state);
   }
 
   void PQnode::display() {
--- src/Model/Power/include/Powernode.hpp
+++ src/Model/Power/include/Powernode.hpp
@@ -15,12 +15,19 @@ namespace Model::Networkproblem::Power {
 
     virtual ~Powernode(){};
 
-    virtual int get_number_of_states() const override final;
+    int get_number_of_states() const final;
 
     double get_G() const;
     double get_B() const;
 
   protected:
+    double P(double new_time, Eigen::VectorXd const &new_state);
+    double Q(double new_time, Eigen::VectorXd const &new_state);
+    void evaluate_P_derivative(Aux::Matrixhandler *jacobianhandler,
+                               Eigen::VectorXd const &new_state);
+    void evaluate_Q_derivative(Aux::Matrixhandler *jacobianhandler,
+                               Eigen::VectorXd const &new_state);
+
     Boundaryvalue<Powernode, 2> boundaryvalue;
     /// Real part of the admittance of this node
     double G;
