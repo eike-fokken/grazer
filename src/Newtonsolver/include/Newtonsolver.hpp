2e5f2561bde9b666da822a62dfaa3370a76bbf00
Eike <e.fokken+git@posteo.de>
2020-11-19 14:22:42 +0100

    Newtonsolver is now templated on the problem class.
    This is for testing purposes. Later on it might help for factoring out this
    class as a nonlinear solver for Eigen.

--- src/Newtonsolver/include/Newtonsolver.hpp
+++ src/Newtonsolver/include/Newtonsolver.hpp
@@ -2,12 +2,9 @@
 #include "Problem.hpp"
 #include <Eigen/Sparse>
 #include <Eigen/SparseLU>
+#include <Matrixhandler.hpp>
 
 namespace Model {}
-
-namespace Aux {
-  class Matrixhandler;
-}
 
 /// \brief This namespace holds tools for solving numerical problems, e.g.
 /// finding the root of a non-linear function.
@@ -29,20 +26,36 @@ namespace Solver {
   ///
   /// This class holds a SparseMatrix and a corresponding Sparse-matrix solver,
   /// so it can compute the solution of a non-linear problem.
-  class Newtonsolver {
+  template <typename Problemtype> class Newtonsolver {
   public:
     Newtonsolver(double _tolerance, int _maximal_iterations)
         : tolerance(_tolerance), maximal_iterations(_maximal_iterations){};
 
-    void evaluate_state_derivative_triplets(Model::Problem &problem,
+    void evaluate_state_derivative_triplets(Problemtype &problem,
                                             double last_time, double new_time,
                                             Eigen::VectorXd const &last_state,
-                                            Eigen::VectorXd const &new_state);
+                                            Eigen::VectorXd const &new_state) {
+      {
+        jacobian.resize(new_state.size(), new_state.size());
+        Aux::Triplethandler handler(&jacobian);
 
-    void evaluate_state_derivative_coeffref(Model::Problem &problem,
+        Aux::Triplethandler *const handler_ptr = &handler;
+        problem.evaluate_state_derivative(handler_ptr, last_time, new_time,
+                                          last_state, new_state);
+        handler.set_matrix();
+      }
+      sparselusolver.analyzePattern(jacobian);
+    };
+
+    void evaluate_state_derivative_coeffref(Problemtype &problem,
                                             double last_time, double new_time,
                                             Eigen::VectorXd const &last_state,
-                                            Eigen::VectorXd const &new_state);
+                                            Eigen::VectorXd const &new_state) {
+      Aux::Coeffrefhandler handler(&jacobian);
+      Aux::Coeffrefhandler *const handler_ptr = &handler;
+      problem.evaluate_state_derivative(handler_ptr, last_time, new_time,
+                                        last_state, new_state);
+    };
 
     /// \brief This method computes a solution to f(new_state) == 0.
     ///
@@ -52,9 +65,80 @@ namespace Solver {
     /// an affine-invariant Newton solution described in chapter 4.2 in
     /// "Deuflhard and Hohmann: Numerical Analysis in Modern Scientific
     /// Computing". Afterwards there should hold f(new_state) == 0.
-    Solutionstruct solve(Eigen::VectorXd &new_state, Model::Problem &problem,
+    Solutionstruct solve(Eigen::VectorXd &new_state, Problemtype &problem,
                          bool new_jacobian_structure, double last_time,
-                         double new_time, Eigen::VectorXd const &last_state);
+                         double new_time, Eigen::VectorXd const &last_state) {
+      Solutionstruct solstruct;
+
+      // If the structure of the jacobian changed one should generate it from
+      // triplets anew:
+      if (new_jacobian_structure) {
+        evaluate_state_derivative_triplets(problem, last_time, new_time,
+                                           last_state, new_state);
+      }
+      // If the non-zero elements are the same, just update them:
+      else {
+        evaluate_state_derivative_coeffref(problem, last_time, new_time,
+                                           last_state, new_state);
+      }
+
+      Eigen::VectorXd temp1(new_state.size());
+      Eigen::VectorXd temp2(new_state.size());
+      Eigen::VectorXd *vec1 = &temp1;
+      Eigen::VectorXd *vec2 = &temp2;
+      Eigen::VectorXd *temp;
+
+      Eigen::VectorXd function(new_state.size());
+      Eigen::VectorXd step(new_state.size());
+      double stepsizeparameter;
+      double oldstepsizeparameter;
+      double stepsize;
+
+      *vec1 = new_state;
+      problem.evaluate(function, last_time, new_time, last_state, *vec1);
+      solstruct.residual = function.lpNorm<Eigen::Infinity>();
+
+      while (solstruct.residual > tolerance &&
+             solstruct.used_iterations < maximal_iterations) {
+        // compute new derivative before the next step:
+        problem.evaluate(function, last_time, new_time, last_state, *vec2);
+        evaluate_state_derivative_coeffref(problem, last_time, new_time,
+                                           last_state, *vec1);
+        sparselusolver.factorize(jacobian);
+        step = sparselusolver.solve(-function);
+        oldstepsizeparameter = step.lpNorm<2>();
+        stepsize = 2.0; // This is set to 2.0, so that in the first run of the
+                        // following loop the stepsize is decreased to the
+                        // correct initial value of 1.0.
+
+        // Stepsize computation:
+        do {
+          stepsize *= 0.5;
+          *vec2 = *vec1 + stepsize * step;
+          problem.evaluate(function, last_time, new_time, last_state, *vec2);
+          stepsizeparameter =
+              sparselusolver.solve(-function).template lpNorm<2>();
+        } while ((stepsizeparameter >
+                  (1 - decrease_value * stepsize) * oldstepsizeparameter) &&
+                 stepsize > minimal_stepsize);
+
+        solstruct.residual = function.lpNorm<Eigen::Infinity>();
+        temp = vec1;
+        vec1 = vec2; // we switch pointers in order to not copy data and still
+                     // have access to the last value.
+        vec2 = temp;
+        ++solstruct.used_iterations;
+      }
+      new_state = *vec1;
+
+      if (solstruct.used_iterations == maximal_iterations) {
+        solstruct.success = false;
+      } else {
+        solstruct.success = true;
+      }
+
+      return solstruct;
+    };
 
   private:
     /// Holds an instance of the actual solver, to save computation time it
