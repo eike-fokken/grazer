c9fe28631a6232d8f50b91a24b46b634c3bcd9b0
Eike <e.fokken+git@posteo.de>
2020-12-05 11:40:31 +0100

    pipe, wip

--- src/Model/Networkproblem/Gas/Pipe.cpp
+++ src/Model/Networkproblem/Gas/Pipe.cpp
@@ -1,1 +1,17 @@
+#include "Implicitboxscheme.hpp"
+#include "Isothermaleulerequation.hpp"
 #include <Pipe.hpp>
+#include <Mathfunctions.hpp>
+
+namespace Model::Networkproblem::Gas {
+
+  Pipe::Pipe(std::string _id, Network::Node *start_node,
+             Network::Node *end_node, nlohmann::ordered_json topology_json)
+      : Gasedge(_id, start_node, end_node),
+        length(topology_json["length"]["value"].get<double>() * 1e3),
+        diameter(topology_json["diameter"]["value"].get<double>() * 1e-3),
+        roughness(topology_json["roughness"]["value"].get<double>()),
+        bl(Balancelaw::Isothermaleulerequation(0.25* Aux::Pi * diameter * diameter , diameter, roughness)),
+        scheme(Model::Scheme::Implicitboxscheme(bl)) { }
+
+}
--- src/Model/Networkproblem/Gas/include/Pipe.hpp
+++ src/Model/Networkproblem/Gas/include/Pipe.hpp
@@ -1,9 +1,53 @@
+#pragma once
 #include <Gasedge.hpp>
-
+#include <Isothermaleulerequation.hpp>
+#include <Implicitboxscheme.hpp>
 
 namespace Model::Networkproblem::Gas {
 
-  class Pipe: public Gasedge {
+  class Pipe final: public Gasedge {
+  public:
+
+    Pipe(std::string _id, Network::Node *start_node, Network::Node *end_node, nlohmann::ordered_json topology_json);
+
+
+
+    void evaluate(Eigen::Ref<Eigen::VectorXd> rootvalues, double last_time,
+                  double new_time, Eigen::VectorXd const &last_state,
+                  Eigen::VectorXd const &new_state) const override;
+    void evaluate_state_derivative(
+                                   Aux::Matrixhandler *jacobianhandler, double last_time, double new_time,
+                                   Eigen::VectorXd const &, Eigen::VectorXd const &new_state) const override;
+
+
+
+    int get_number_of_states() const override;
+
+    void
+    print_to_files(std::filesystem::path const &output_directory) override;
+
+    void save_values(double time, Eigen::VectorXd const &state) override;
+
+    void set_initial_values(Eigen::VectorXd &new_state,
+                            nlohmann::ordered_json initial_json) override;
+
+
+    Eigen::Vector2d get_boundary_p_qvol(int direction, Eigen::VectorXd const &state) const override;
+
+
+    void dboundary_p_qvol_dstate(int direction, Aux::Matrixhandler * jacobianhandler, Eigen::RowVector2d function_derivative, int rootvalues_index, Eigen::VectorXd const &state) const override;
+
+
+
+
+  private:
+
+    double const length;
+    double const diameter;
+    double const roughness;
+
+    Balancelaw::Isothermaleulerequation bl;
+    Model::Scheme::Implicitboxscheme scheme;
 
 
   };
