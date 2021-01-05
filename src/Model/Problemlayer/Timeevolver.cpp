d8f963342eff89799c1e5a9517cc26d70851e6ad
Eike <e.fokken+git@posteo.de>
2021-01-05 13:06:35 +0100

    time evolver, wip

--- src/Model/Problemlayer/Timeevolver.cpp
+++ src/Model/Problemlayer/Timeevolver.cpp
@@ -1,20 +1,61 @@
+#include "Newtonsolver.hpp"
+#include "nlohmann/json.hpp"
+#include <Problem.hpp>
 #include <Timeevolver.hpp>
+#include <iostream>
 
 namespace Model {
+  
+  Timedata::Timedata(double _starttime, double _endtime, double desired_delta_t):
+                     starttime(_starttime), endtime(_endtime),
+                     Full_time_interval(endtime - starttime),
+                     Number_of_timesteps(init_Number_of_timesteps(desired_delta_t)),
+                     delta_t(init_delta_t())
+                     {}
 
-  Timeevolver::Timeevolver(double _starttime, double _endtime,
-                           double desired_delta_t)
-      : starttime(_starttime), endtime(_endtime),
-        Full_time_interval(endtime - starttime),Number_of_timesteps(init_Number_of_timesteps(desired_delta_t)),delta_t(init_delta_t())
-  {}
-
-  int Timeevolver::init_Number_of_timesteps(double desired_delta_t) const {
+  int Timedata::init_Number_of_timesteps(double desired_delta_t) const {
     int const _Number_of_timesteps =
         static_cast<int>(std::ceil(Full_time_interval / desired_delta_t));
     return _Number_of_timesteps;
   }
 
-  double Timeevolver::init_delta_t() const {
+  double Timedata::init_delta_t() const {
     return Full_time_interval / Number_of_timesteps;
   }
+
+  Timeevolver::Timeevolver(double tolerance,
+                           int maximal_number_of_iterations)
+    : solver(tolerance,maximal_number_of_iterations) {}
+
+
+
+  void Timeevolver::simulate(Timedata timedata, Model::Problem & problem,
+                             int number_of_states, json problem_initial_json) {
+    double last_time = timedata.starttime;
+    Eigen::VectorXd last_state(number_of_states);
+
+    problem.set_initial_values(last_state,problem_initial_json);
+    problem.save_values(last_time, last_state);
+
+    double new_time = last_time+ timedata.delta_t;
+
+    Eigen::VectorXd new_state(last_state);
+
+    for (int i = 0; i != timedata.Number_of_timesteps; ++i) {
+      last_state = new_state;
+      new_time = last_time + timedata.delta_t;
+
+      auto solstruct = solver.solve(new_state, problem, true, last_time,
+                                    new_time, last_state);
+      problem.save_values(new_time, new_state);
+
+      std::cout << new_time << ": ";
+      std::cout << solstruct.residual << ", ";
+      std::cout << solstruct.used_iterations << std::endl;
+
+      last_time = new_time;
+
+    }
+  }
+
 }
--- src/Model/Problemlayer/include/Timeevolver.hpp
+++ src/Model/Problemlayer/include/Timeevolver.hpp
@@ -1,42 +1,49 @@
+#include "nlohmann/json.hpp"
 #include <Eigen/Sparse>
+#include <Newtonsolver.hpp>
+#include <memory>
 #include <utility>
 
+// Forward declarations:
 namespace Model {
+  class Problem;
+}
 
-  class Timeevolver {
-  public:
+namespace Model {
+  using json = nlohmann::ordered_json;
 
-    Timeevolver(double starttime, double endtime, double desired_delta_t);
+  struct Timedata{
 
-
-    void simulate();
-
-    void make_step();
-
-
-
-  private:
-
-    /// Put in cpp and maybe refactor to free helper function
-
-    int init_Number_of_timesteps (double desired_delta_t) const ;
-
-    double init_delta_t() const ;
-
-    Eigen::VectorXd new_state;
-    Eigen::VectorXd last_state;
+    Timedata() = delete;
+    Timedata(double _starttime, double _endtime, double desired_delta_t);
 
 
-    // Jsonreader::set_initial_values(state1, initial, p);
-
-    double last_time;
-    double new_time;
     double const starttime;
     double const endtime;
-    double const Full_time_interval{endtime - starttime};
+    double const Full_time_interval;
     int const Number_of_timesteps;
+
+    // maybe later on this should be made non-const for variable stepsizes:
     double const delta_t;
-    
+
+  private:
+    int init_Number_of_timesteps(double desired_delta_t) const;
+
+    double init_delta_t() const;
   };
 
-}
+
+  class Timeevolver {
+
+  public:
+    Timeevolver() = delete;
+    Timeevolver(double tolerance, int maximal_number_of_iterations);
+
+    void simulate(Timedata timedata, Model::Problem & problem,
+                  int number_of_states, json problem_initial_json);
+
+  private :
+        Solver::Newtonsolver solver;
+    };
+
+} // namespace Model
