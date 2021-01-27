0fdf13b27b6bf8b906e738078aa250a156542fa7
Eike <e.fokken+git@posteo.de>
2021-01-27 22:33:01 +0100

    made a new library instead of changing json reader

---
+++ src/Aux/aux_json/Aux_json.cpp
@@ -0,0 +1,33 @@
+
+namespace aux_json {
+
+  nlohmann::json get_json_from_string(std::string const &json_string) {
+    nlohmann::json json_object;
+    auto json_path = std::filesystem::path(json_string);
+    if(!std::filesystem::exists((json_path))){ gthrow({"The file ",json_path.string(), " does not exist!"});}
+    try {
+      std::ifstream jsonfilestream(json_path);
+      jsonfilestream >> json_object;
+    } catch (...) {
+      std::cout << __FILE__ << ":" << __LINE__
+                << ": Couldn't load json from file: " << json_string
+                << std::endl;
+      throw;
+    }
+    return json_object;
+  }
+
+  void replace_entry_with_json_from_file(nlohmann::json & super_json, std::string const & key) {
+
+    auto & sub_json = super_json["key"];
+    if(sub_json.is_object()){
+      return;
+    } else if(sub_json.is_string()){
+      auto new_sub_json = get_json_from_string(sub_json.get<std::string>());
+    } else {
+      gthrow({"The value at \"", key, "\" is neither a valid json object, nor a "
+          "string pointing to a json file."});
+    }
+  }
+
+} // namespace aux_json
---
+++ src/Aux/aux_json/include/Aux_json.hpp
@@ -0,0 +1,5 @@
+
+
+void replace_entry_with_json_from_file(nlohmann::json &super_json,
+                                       std::string const &key);
+nlohmann::json get_json_from_string(std::string const &json_string);
