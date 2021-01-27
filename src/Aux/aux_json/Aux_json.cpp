7c07d8e15e21df992f70ac91deddaa64b7914d60
Eike <e.fokken+git@posteo.de>
2021-01-27 22:57:07 +0100

    aux_json, wip

--- src/Aux/aux_json/Aux_json.cpp
+++ src/Aux/aux_json/Aux_json.cpp
@@ -1,21 +1,11 @@
-
+#include <Aux_json.hpp>
+#include <Exception.hpp>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
 namespace aux_json {
 
-  nlohmann::json get_json_from_string(std::string const &json_string) {
-    nlohmann::json json_object;
-    auto json_path = std::filesystem::path(json_string);
-    if(!std::filesystem::exists((json_path))){ gthrow({"The file ",json_path.string(), " does not exist!"});}
-    try {
-      std::ifstream jsonfilestream(json_path);
-      jsonfilestream >> json_object;
-    } catch (...) {
-      std::cout << __FILE__ << ":" << __LINE__
-                << ": Couldn't load json from file: " << json_string
-                << std::endl;
-      throw;
-    }
-    return json_object;
-  }
 
   void replace_entry_with_json_from_file(nlohmann::json & super_json, std::string const & key) {
 
@@ -28,6 +18,24 @@ namespace aux_json {
       gthrow({"The value at \"", key, "\" is neither a valid json object, nor a "
           "string pointing to a json file."});
     }
+  }
+
+  nlohmann::json get_json_from_string(std::string const &json_string) {
+    nlohmann::json json_object;
+    auto json_path = std::filesystem::path(json_string);
+    if (!std::filesystem::exists((json_path))) {
+      gthrow({"The file ", json_path.string(), " does not exist!"});
+    }
+    try {
+      std::ifstream jsonfilestream(json_path);
+      jsonfilestream >> json_object;
+    } catch (...) {
+      std::cout << __FILE__ << ":" << __LINE__
+                << ": Couldn't load json from file: " << json_string
+                << std::endl;
+      throw;
+    }
+    return json_object;
   }
 
 } // namespace aux_json
--- src/Aux/aux_json/include/Aux_json.hpp
+++ src/Aux/aux_json/include/Aux_json.hpp
@@ -1,5 +1,19 @@
+#include <nlohmann/json.hpp>
+
+namespace aux_json {
+  /// \brief replaces a filename string with the json from the file
+  ///
+  /// Checks whether super_json["key"] holds a string. If so, checks that the
+  /// string is a valid file name, reads the contents of the file of that name
+  /// into a json object and replaces super_json["key"] with this json object.
+  void replace_entry_with_json_from_file(nlohmann::json &super_json,
+                                         std::string const &key);
+
+  /// \brief Constructs a json object from a file.
+  ///
+  ///
+  nlohmann::json get_json_from_string(std::string const &json_string);
 
 
-void replace_entry_with_json_from_file(nlohmann::json &super_json,
-                                       std::string const &key);
-nlohmann::json get_json_from_string(std::string const &json_string);
+
+}
