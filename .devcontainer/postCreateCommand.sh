daa61471e6255448178d71078f30fc713b04f696
Felix Benning <felix.benning@d-fine.de>
2021-02-02 11:21:02 +0000

    echo status

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+echo "copying mounted ssh folder into ~/.ssh"
 # see https://stackoverflow.com/questions/27069193/how-can-i-mount-a-users-existing-ssh-directory-into-a-docker-container
 # and https://nickjanetakis.com/blog/docker-tip-56-volume-mounting-ssh-keys-into-a-docker-container
 sudo rm --recursive ~/.ssh
@@ -7,3 +8,5 @@ cp -R /tmp/.mounted_ssh ~/.ssh
 sudo chmod 700 ~/.ssh
 sudo chmod 644 ~/.ssh/id_rsa.pub
 sudo chmod 600 ~/.ssh/id_rsa
+
+echo "finished copying"
