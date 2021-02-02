b4f110ad132e2438de764d909570f273068d221c
Felix Benning <felix.benning@d-fine.de>
2021-02-02 10:44:03 +0000

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# see https://stackoverflow.com/questions/27069193/how-can-i-mount-a-users-existing-ssh-directory-into-a-docker-container
+# and https://nickjanetakis.com/blog/docker-tip-56-volume-mounting-ssh-keys-into-a-docker-container
+sudo rm --recursive ~/.ssh
+cp -R /tmp/.mounted_ssh ~/.ssh
+sudo chmod 700 ~/.ssh
+sudo chmod 644 ~/.ssh/id_rsa.pub
+sudo chmod 600 ~/.ssh/id_rsa
