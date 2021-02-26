06de4fe92bf1772f6fb35f9e868ff1d8646e9abf
Eike <e.fokken+git@posteo.de>
2021-02-26 10:54:34 +0100

    bugfix in file names for printout

--- src/Model/Networkproblem/Gas/Shortcomponent.cpp
+++ src/Model/Networkproblem/Gas/Shortcomponent.cpp
@@ -8,7 +8,7 @@ namespace Model::Networkproblem::Gas {
 
   void Shortcomponent::print_helper(std::filesystem::path const &output_directory, std::string const & type) {
 
-    std::string prestring = "Gas_"+type;
+    std::string prestring = "Gas_"+type+"_";
     std::string pressure_file_name = (get_id().insert(0, prestring))+"_p";
     std::string flow_file_name =(get_id().insert(0, prestring))+"_q";
     std::filesystem::path shortcomponent_output_pressure(output_directory /
