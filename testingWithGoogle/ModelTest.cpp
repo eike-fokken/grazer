41f75c9a971206f8573653911b5d9b32e833f41e
Felix Benning <felix.benning@gmail.com>
2021-03-03 15:18:48 +0100

    get ModelTest working again

--- testingWithGoogle/ModelTest.cpp
+++ testingWithGoogle/ModelTest.cpp
@@ -1,4 +1,3 @@
-#include <Aux_json.hpp>
 #include "MockSubproblem.hpp"
 #include "Problem.hpp"
 #include <gtest/gtest.h>
@@ -19,38 +18,50 @@ TEST(modelSubproblem, Model_evaluate) {
   auto output_dir = std::filesystem::path("");
   Model::Problem problem(problem_json, output_dir);
 
-//   // make unique pointer of mocksub1 and mocksub2
-//   auto mock1_ptr = std::make_unique<GrazerTest::MockSubproblem>();
-//   auto mock2_ptr = std::make_unique<GrazerTest::MockSubproblem>();
+  // make unique pointer of mocksub1 and mocksub2
+  auto mock1_ptr = std::make_unique<GrazerTest::MockSubproblem>();
+  auto mock2_ptr = std::make_unique<GrazerTest::MockSubproblem>();
 
-//   // add subproblem to problem
-//   problem.add_subproblem(std::move(mock1_ptr));
-//   problem.add_subproblem(std::move(mock2_ptr));
+  // add subproblem to problem
+  problem.add_subproblem(std::move(mock1_ptr));
+  problem.add_subproblem(std::move(mock2_ptr));
 
-//   // call problem.evaluate
-//   double last_time(0.0);
-//   double new_time(1.0);
-//   Eigen::VectorXd rootvalues(2);
-//   Eigen::VectorXd v1(2);
-//   v1(0) = 2;
-//   v1(1) = 3;
-//   Eigen::VectorXd v2(2);
-//   v1(0) = 3;
-//   v1(1) = 4;
+  // call problem.evaluate
+  double last_time(0.0);
+  double new_time(1.0);
+  Eigen::VectorXd rootvalues(2);
+  Eigen::VectorXd v1(2);
+  v1[0] = 2;
+  v1[1] = 3;
+  Eigen::VectorXd v2(2);
+  v1[0] = 3;
+  v1[1] = 4;
 
-//   // This is necessary for the expect_call to work properly...
-//   // Eigen::Ref<Eigen::VectorXd> rootref(rootvalues);
+  // This is necessary for the expect_call to work properly...
+  Eigen::Ref<Eigen::VectorXd> rootref(rootvalues);
 
-//   //  // expect the call to evaluate on the subproblems.
-//   //  // The cast magic is necessary to have the right type at hand...
-//   EXPECT_CALL(
-//       *dynamic_cast<GrazerTest::MockSubproblem *>(problem.get_subproblems()[0]),
-//       evaluate(Eigen::Ref<Eigen::VectorXd>(rootvalues), last_time, new_time, Eigen::Ref<Eigen::VectorXd const>(v1), Eigen::Ref<Eigen::VectorXd const> (v2)))
-//       .Times(1);
-//   EXPECT_CALL(
-//       *dynamic_cast<GrazerTest::MockSubproblem *>(problem.get_subproblems()[1]),
-//       evaluate(Eigen::Ref<Eigen::VectorXd>(rootvalues), last_time, new_time, Eigen::Ref<Eigen::VectorXd const>(v1), Eigen::Ref<Eigen::VectorXd const> (v2)))
-//       .Times(1);
+   // expect the call to evaluate on the subproblems.
+   // The cast magic is necessary to have the right type at hand...
+  EXPECT_CALL(
+    *dynamic_cast<GrazerTest::MockSubproblem *>(problem.get_subproblems()[0]),
+    evaluate(
+      Eigen::Ref<Eigen::VectorXd>(rootvalues), 
+      last_time, 
+      new_time, 
+      Eigen::Ref<Eigen::VectorXd const>(v1), 
+      Eigen::Ref<Eigen::VectorXd const>(v2)
+    )
+  ).Times(1);
+  EXPECT_CALL(
+    *dynamic_cast<GrazerTest::MockSubproblem *>(problem.get_subproblems()[1]),
+    evaluate(
+      Eigen::Ref<Eigen::VectorXd>(rootvalues),
+      last_time,
+      new_time,
+      Eigen::Ref<Eigen::VectorXd const>(v1),
+      Eigen::Ref<Eigen::VectorXd const>(v2)
+    )
+  ).Times(1);
 
-//   problem.evaluate(rootvalues, last_time, new_time, v1, v2);
+  problem.evaluate(rootvalues, last_time, new_time, v1, v2);
 }
