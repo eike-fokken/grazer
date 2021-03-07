3c363a4b3ab0fb38065859d7507fb9c52b60b163
Felix Benning <felix.benning@gmail.com>
2021-03-07 20:20:15 +0100

    add doxygen docstrings

--- src/validation/include/json_validation.hpp
+++ src/validation/include/json_validation.hpp
@@ -2,6 +2,24 @@
 #include <nlohmann/json.hpp>
 
 namespace nlohmann {
+  /**
+   * @brief assumes that the json data has a "$schema" key which points to
+   * a valid json schema to validate the data against
+   * 
+   * @param data json to validate
+   * @return true 
+   * @return false 
+   */
   bool validate_json(nlohmann::json const &data);
-  bool validate_json(nlohmann::json const &data, std::string const &location);
+
+  /**
+   * @brief validates the json data against the json schema found at schema_location
+   * 
+   * @param data json to validate
+   * @param schema_location path to the json schema to validate against
+   * @return true if validation succeeded
+   * @return false (should never happen as the function will throw an error)
+   * 
+   */
+  bool validate_json(nlohmann::json const &data, std::string const &schema_location);
 }
