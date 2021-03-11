e358fd02f54c920f20228afafdfc47a975a27838
Felix Benning <felix.benning@gmail.com>
2021-03-11 22:09:37 +0100

    add documentation where possible

--- src/Auxillary/unit_conversion/include/unit_conversion.hpp
+++ src/Auxillary/unit_conversion/include/unit_conversion.hpp
@@ -51,7 +51,10 @@ namespace Aux::unit {
     {"lb", 0.45359237}
   };
 
+  // arbitrary lambda conversions
   typedef std::function<double (double)> conversion;
+
+  // conversion instructions for temperature
   const std::map<std::string, conversion> temperature_units{
     {"K", [](double x) { return x; }},
     {"C", [](double x) { return x + 273.15; }},
@@ -67,7 +70,7 @@ namespace Aux::unit {
     json const &unit_json,
     std::map<std::string, Conversiontype> const &unit_map
   );
-
+  
   extern template
   double parse_to_si<conversion>(
     json const &unit_json,
@@ -81,13 +84,63 @@ namespace Aux::unit {
     std::map<std::string, double> const &unit_map
   );
 
-  double unit_conversion(double value, double unit);
+
+  /**
+   * @brief In contrast to multiplicative conversions (e.g. feet to meter)
+   * temperature conversion is more complicated. To allow simple multiplication
+   * tables unit_conversion is overloaded to accept either a double or a lambda
+   * function.
+   * 
+   * unit_conversion behaves like value*conv if conv is a double which covers
+   * most unit conversions.
+   * 
+   * unit_conversion behaves like conv(value) if conv is a lambda which allows
+   * for arbitarily complicated conversion.
+   * 
+   * @param value of the current unit
+   * @param conv multiplicator to get to the si-unit
+   * @return double si-unit
+   */
+  double unit_conversion(double value, double conv);
+
+  /**
+   * @brief In contrast to multiplicative conversions (e.g. feet to meter)
+   * temperature conversion is more complicated. To allow simple multiplication
+   * tables unit_conversion is overloaded to accept either a double or a lambda
+   * function.
+   * 
+   * unit_conversion behaves like value*conv if conv is a double which covers
+   * most unit conversions.
+   * 
+   * unit_conversion behaves like conv(value) if conv is a lambda which allows
+   * for arbitarily complicated conversion.
+   * 
+   * @param value value which might have an arbitrary unit
+   * @param conv lambda function which returns the si-unit
+   * @return double si-unit
+   */
   double unit_conversion(double value, conversion conv);
 
+  /**
+   * @brief parses a string which is the prefix of a unit (e.g. "500 k" of "500 km")
+   * returns the value of the number multiplied with the value of the symbol which
+   * is looked up in the prefix_map (e.g. an si prefix map (or a binary prefix map))
+   * 
+   * @param prefix the prefix string to parse
+   * @param prefix_map the prefix_map which includes all the possible symbols
+   * @return double the prefix value
+   */
   double parse_prefix(
     std::string const &prefix, std::map<std::string, double> const &prefix_map
   );
 
+  /**
+   * @brief parses a string which is the prefix of a unit (e.g. "500 k" of "500 km")
+   * returns the value of the number multiplied with the value of the si prefix
+   * 
+   * @param prefix the prefix string to parse
+   * @return double the prefix value
+   */
   double parse_prefix_si(std::string const &prefix);
 
   template<typename T> const std::tuple<std::string, T> parse_unit(
