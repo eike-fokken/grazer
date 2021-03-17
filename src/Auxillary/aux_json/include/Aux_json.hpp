a18a8481f7d6381107ba326d9d78fb7a68216bc3
Eike <e.fokken+git@posteo.de>
2021-03-17 11:01:25 +0100

    added a bit more docs

--- src/Auxillary/aux_json/include/Aux_json.hpp
+++ src/Auxillary/aux_json/include/Aux_json.hpp
@@ -8,12 +8,13 @@ namespace aux_json {
   /// Checks whether super_json["key"] holds a string. If so, checks that the
   /// string is a valid file name, reads the contents of the file of that name
   /// into a json object and replaces super_json["key"] with this json object.
+  /// if super_json["key"] is a json object, does nothing.
   void replace_entry_with_json_from_file(nlohmann::json &super_json,
                                          std::string const & key);
 
   /// \brief Constructs a json object from a file.
   ///
-  ///
+  /// @throws std::runtime_error if the file is not present or if is not valid json.
   nlohmann::json get_json_from_file_path(std::filesystem::path const &file_path);
 
 
