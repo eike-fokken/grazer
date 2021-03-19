1e8a245cc10b5dcc427955fc3c96bf5e6ab6c5b7
Felix Benning <felix.benning@gmail.com>
2021-03-19 11:18:04 +0100

    remove legacy gas_factory

--- src/Model/Componentfactory/Gas_factory/Gas_factory.cpp
+++ src/Model/Componentfactory/Gas_factory/Gas_factory.cpp
@@ -23,35 +23,4 @@ namespace Model::Componentfactory {
     factory.add_edge_type(std::make_unique<EdgeType<Networkproblem::Gas::Gaspowerconnection>>());
   }
 
-  std::map<std::string, Nodefactory> Gas_factory::get_nodetypemap_impl(){
-
-    std::map<std::string, Nodefactory> data;
-        std::vector<std::unique_ptr<AbstractNodeType>> buildervector;
-
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Gas::Source>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Gas::Sink>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Gas::Innode>>());
-
-    for (auto const &builder : buildervector) {
-      data.insert({builder->get_name(), builder->get_factory()});
-    }
-
-    return data;
-  }
-  std::map<std::string, Edgefactory> Gas_factory::get_edgetypemap_impl(){
-    std::map<std::string, Edgefactory> data;
-    std::vector<std::unique_ptr<AbstractEdgeType>> buildervector;
-
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Pipe>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Shortpipe>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Controlvalve>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Compressorstation>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Gaspowerconnection>>());
-
-    for (auto const &builder : buildervector) {
-      data.insert({builder->get_name(), builder->get_factory()});
-    }
-    return data;
-  }
-
 }
--- src/Model/Componentfactory/Gas_factory/include/Gas_factory.hpp
+++ src/Model/Componentfactory/Gas_factory/include/Gas_factory.hpp
@@ -10,11 +10,4 @@ namespace Model::Componentfactory {
    */
   void add_gas_components(Componentfactory &factory);
 
-  /// \brief Provides factories for gas component types.
-  struct Gas_factory : public Componentfactory_interface<Gas_factory> {
-
-    std::map<std::string, Nodefactory> get_nodetypemap_impl();
-    std::map<std::string, Edgefactory> get_edgetypemap_impl();
-
-  };
 }
