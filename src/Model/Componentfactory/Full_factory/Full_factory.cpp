06e096fd0aa349c4af161e25709956cefd7b9d53
Felix Benning <felix.benning@gmail.com>
2021-03-19 11:19:20 +0100

    remove legacy full_factory

--- src/Model/Componentfactory/Full_factory/Full_factory.cpp
+++ src/Model/Componentfactory/Full_factory/Full_factory.cpp
@@ -23,39 +23,4 @@ namespace Model::Componentfactory {
     add_gas_components(factory);
   }
 
-  std::map<std::string, Nodefactory> Full_factory::get_nodetypemap_impl(){
-
-    std::map<std::string, Nodefactory> data;
-        std::vector<std::unique_ptr<AbstractNodeType>> buildervector;
-
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Power::Vphinode>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Power::PVnode>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Power::PQnode>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Gas::Source>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Gas::Sink>>());
-    buildervector.push_back(std::make_unique<NodeType<Networkproblem::Gas::Innode>>());
-
-    for (auto const &builder : buildervector) {
-      data.insert({builder->get_name(), builder->get_factory()});
-    }
-
-    return data;
-  }
-  std::map<std::string, Edgefactory> Full_factory::get_edgetypemap_impl(){
-    std::map<std::string, Edgefactory> data;
-    std::vector<std::unique_ptr<AbstractEdgeType>> buildervector;
-
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Power::Transmissionline>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Pipe>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Shortpipe>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Controlvalve>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Compressorstation>>());
-    buildervector.push_back(std::make_unique<EdgeType<Networkproblem::Gas::Gaspowerconnection>>());
-
-    for (auto const &builder : buildervector) {
-      data.insert({builder->get_name(), builder->get_factory()});
-    }
-    return data;
-  }
-
 }
--- src/Model/Componentfactory/Full_factory/include/Full_factory.hpp
+++ src/Model/Componentfactory/Full_factory/include/Full_factory.hpp
@@ -11,11 +11,4 @@ namespace Model::Componentfactory {
    */
   void add_all_components(Componentfactory &factory);
 
-  /// \brief Provides factories for all types of Networkcomponents defined in grazer.
-  struct Full_factory : public Componentfactory_interface<Full_factory> {
-
-    std::map<std::string, Nodefactory> get_nodetypemap_impl();
-    std::map<std::string, Edgefactory> get_edgetypemap_impl();
-
-  };
 }
