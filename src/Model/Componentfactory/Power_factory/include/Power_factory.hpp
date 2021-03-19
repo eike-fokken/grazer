b1427b5d28fcd4e68abfd814b2b470b6cc6359b3
Felix Benning <felix.benning@gmail.com>
2021-03-19 11:16:35 +0100

    remove legacy Power_factory

--- src/Model/Componentfactory/Power_factory/include/Power_factory.hpp
+++ src/Model/Componentfactory/Power_factory/include/Power_factory.hpp
@@ -10,16 +10,4 @@ namespace Model::Componentfactory {
    * @param factory to which to add the Power Components
    */
   void add_power_components(Componentfactory &factory);
-
-  struct PowerFactory : public Componentfactory {
-    PowerFactory();
-  };
-
-  /// \brief Provides factories for power component types.
-  struct Power_factory : public Componentfactory_interface<Power_factory> {
-
-    std::map<std::string, Nodefactory> get_nodetypemap_impl();
-    std::map<std::string, Edgefactory> get_edgetypemap_impl();
-
-  };
 }
