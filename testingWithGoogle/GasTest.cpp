b931a8c0cb53857ce3c966e4216e9779e5357b47
Eike <e.fokken+git@posteo.de>
2021-03-21 07:41:37 +0100

    templates for make_value_json are now done.

--- testingWithGoogle/GasTest.cpp
+++ testingWithGoogle/GasTest.cpp
@@ -40,11 +40,13 @@ namespace Eigen {
   void to_json(nlohmann::json &j, const MatrixBase<Derived> &eig);
 }
 
+// this is for T = double or T = Eigen::Vector...
 template <typename T>
 nlohmann::json make_value_json(std::string id, std::string key,
                                T &condition0,
                                T &condition1);
 
+// this is for T = double or T = Eigen::Vector...
 template <typename T>
 nlohmann::json
 make_value_json(std::string id, std::string key,
@@ -868,8 +870,6 @@ TEST_F(GasTEST, Innode_evaluate_state_derivative) {
       Eigen::VectorXd new_state(number_of_variables);
 
       netprob->set_initial_values(new_state, net_initial);
-      (void) last_time;
-      (void) new_time;
   }
             // Eigen::VectorXd rootvalues(number_of_variables);
             // Eigen::VectorXd rootvalues0(number_of_variables);
@@ -1338,83 +1338,68 @@ TEST_F(GasTEST, Innode_evaluate_state_derivative) {
         //   // // }
         // }
 
-      template <typename Derived>
-        void Eigen::to_json(nlohmann::json & j,
-                            const MatrixBase<Derived> &eig) {
-      EIGEN_STATIC_ASSERT_VECTOR_ONLY(MatrixBase<Derived>);
-      // static_assert(eig.cols() == 1,
-      //               "This function is only for vectors, not for
-      //               matrices.");
-      j = nlohmann::json::array();
+template <typename Derived>
+void Eigen::to_json(nlohmann::json & j,
+                    const MatrixBase<Derived> &eig) {
+  EIGEN_STATIC_ASSERT_VECTOR_ONLY(MatrixBase<Derived>);
+  // static_assert(eig.cols() == 1,
+  //               "This function is only for vectors, not for
+  //               matrices.");
+  j = nlohmann::json::array();
 
-      for (Eigen::Index i = 0; i != eig.rows(); ++i) {
-        j.push_back(eig(i));
-      }
+  for (Eigen::Index i = 0; i != eig.rows(); ++i) {
+    j.push_back(eig(i));
+  }
+}
+
+template <typename T>
+nlohmann::json make_value_json(std::string id, std::string key,
+                               T &condition0, T &condition1){
+  nlohmann::json bound;
+  bound["id"] = id;
+  bound["data"] = nlohmann::json::array();
+  nlohmann::json b0;
+  nlohmann::json b1;
+  b1[key] = 1;
+  b0[key] = 0;
+  if constexpr (std::is_same_v<T, double>) {
+    b0["values"].push_back(condition0);
+    b1["values"].push_back(condition1);
+  } else {
+    b0["values"] = condition0;
+    b1["values"] = condition1;
+  }
+  bound["data"].push_back(b0);
+  bound["data"].push_back(b1);
+  return bound;
+}
+
+
+
+template <typename T>
+nlohmann::json make_value_json(std::string id, std::string key,
+                               std::vector<std::pair<double, T>> pairs){
+
+  if (pairs.empty()) {
+    throw std::runtime_error("Pairs empty.");
+  }
+  nlohmann::json bound;
+  bound["id"] = id;
+  bound["data"] = nlohmann::json::array();
+
+
+  for (auto [x, values] : pairs) {
+    nlohmann::json b;
+    b[key] = x;
+    if constexpr (std::is_same_v<T, double>) {
+      b["values"].push_back(values);
+    } else {
+      b["values"] = values;
     }
-    template <typename T>
-    nlohmann::json make_value_json(std::string id, std::string key,
-                                   T &condition0, T &condition1){
-      nlohmann::json bound;
-      bound["id"] = id;
-      bound["data"] = nlohmann::json::array();
-      if constexpr(std::is_same_v<T,double>){
-        nlohmann::json b0;
-        b0[key] = 0;
-        b0["values"].push_back(condition0);
-        nlohmann::json b1;
-        b1[key] = 1;
-        b1["values"].push_back(condition1);
-        bound["data"].push_back(b0);
-        bound["data"].push_back(b1);
-
-      }
-      else{
-        EIGEN_STATIC_ASSERT_VECTOR_ONLY(T);
-        nlohmann::json b0;
-        b0[key] = 0;
-        b0["values"] = condition0;
-        nlohmann::json b1;
-        b1[key] = 1;
-        b1["values"] = condition1;
-        bound["data"].push_back(b0);
-        bound["data"].push_back(b1);
-      }
-      return bound;
-    }
-
-    
-
-    template <typename T>
-    nlohmann::json make_value_json(std::string id, std::string key,
-                                   std::vector<std::pair<double, T>> pairs){
-      
-        if (pairs.empty()) {
-          throw std::runtime_error("Pairs empty.");
-        }
-        nlohmann::json bound;
-        bound["id"] = id;
-        bound["data"] = nlohmann::json::array();
-
-        if constexpr(std::is_same_v<T, double>){
-          for (auto [x, values] : pairs) {
-            nlohmann::json b;
-            b[key] = x;
-            b["values"] = values;
-            bound["data"].push_back(b);
-          }
-        }
-        else {
-        EIGEN_STATIC_ASSERT_VECTOR_ONLY(T);
-
-      for (auto [x, values] : pairs) {
-        nlohmann::json b;
-        b[key] = x;
-        b["values"] = values;
-        bound["data"].push_back(b);
-      }
-        }
-      return bound;
-      }
+    bound["data"].push_back(b);
+  }
+  return bound;
+}
 
 
 nlohmann::json make_initial_json(std::vector<nlohmann::json> shortpipes,
