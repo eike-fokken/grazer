9e832cbd5760f24c31c1d84bdbc0603a6cc9ba27
Eike <e.fokken+git@posteo.de>
2021-03-24 00:08:42 +0100

    moved create topology schema to helper functions.

--- helper_functions/create_topology_schema.cpp
+++ helper_functions/create_topology_schema.cpp
@@ -1,7 +1,6 @@
-
-#include <Componentfactory.hpp>
+#include "Componentfactory.hpp"
+#include "Full_factory.hpp"
 #include <iostream>
-#include <Full_factory.hpp>
 
 using Model::Componentfactory::Componentfactory;
 using Model::Componentfactory::add_all_components;
@@ -9,5 +8,5 @@ using Model::Componentfactory::add_all_components;
 int main() {
   Componentfactory full_factory;
   add_all_components(full_factory);
-  std::cout << full_factory.get_topology_schema().dump() << std::endl;  
+  std::cout << full_factory.get_topology_schema().dump(4) << std::endl;  
 }
