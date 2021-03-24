b04e4aee75f4763c45f35b2a3c2b60d78431dde7
Tibor Cornelli <tibor.cornelli@gmail.com>
2021-03-24 16:16:44 +0100

    Added one test for Input_output

--- testingWithGoogle/InputOutputTest.cpp
+++ testingWithGoogle/InputOutputTest.cpp
@@ -6,7 +6,20 @@
 
 TEST(Input_output, absolute_file_path_in_root) {
 
+  auto problem_root_path_true = std::filesystem::path("grazer/src/directory");
+  auto problem_root_path_false = std::filesystem::path("grazer/src/../directory");
+  auto filepath = std::filesystem::path("subdirectory/file");
 
-  // std::filesystem problem_root_path = (../);
+  EXPECT_TRUE(
+      Aux_executable::absolute_file_path_in_root(problem_root_path_true,filepath));
+  EXPECT_FALSE(
+      Aux_executable::absolute_file_path_in_root(problem_root_path_false, filepath));
 
-}
+};
+
+TEST(Input_output, make_argument_vector) {
+
+
+
+
+};
