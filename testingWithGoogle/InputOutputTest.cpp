7b6a8379eb57db7e41fad42f9d0f406afb1caec5
Tibor Cornelli <tibor.cornelli@gmail.com>
2021-03-24 11:46:03 +0100

    Testing the Input_output file

---
+++ testingWithGoogle/InputOutputTest.cpp
@@ -0,0 +1,12 @@
+#include "Input_output.hpp"
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include <filesystem>
+#include <nlohmann/json.hpp>
+
+TEST(Input_output, absolute_file_path_in_root) {
+
+
+  // std::filesystem problem_root_path = (../);
+
+}
