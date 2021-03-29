1694de3454a1e7c2cd46af3297f725ec46df6368
Eike <e.fokken+git@posteo.de>
2021-03-29 19:11:03 +0200

    Now I understand randutils much better.

---
+++ src/Auxillary/Mathfunctions/Normaldistribution.cpp
@@ -0,0 +1,49 @@
+#include "Normaldistribution.hpp"
+#include "pcg_extras.hpp"
+#include "randutils.hpp"
+#include <bits/stdint-uintn.h>
+#include <cmath>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <random>
+#include <string>
+
+namespace Aux {
+
+  pcg64 setup_random_number_generator() {
+
+    // std::random_device ro;
+
+    // auto args
+    //     = {static_cast<uint32_t>(std::chrono::high_resolution_clock::now()
+    //                                  .time_since_epoch()
+    //                                  .count()),
+    //        static_cast<uint32_t>(ro()),
+    //        static_cast<uint32_t>(ro()),
+    //        static_cast<uint32_t>(ro()),
+    //        static_cast<uint32_t>(ro()),
+    //        static_cast<uint32_t>(ro()),
+    //        static_cast<uint32_t>(ro()),
+    //        static_cast<uint32_t>(ro())};
+    // randutils::auto_seed_256 seed_source(args);
+    randutils::auto_seed_256 seed_source;
+    pcg64 rng(seed_source);
+    return rng;
+  }
+
+  Normaldistribution::Normaldistribution() {}
+
+  double
+  Normaldistribution::operator()(double mean, double standard_deviation) {
+    std::normal_distribution<>::param_type parameters(mean, standard_deviation);
+    return dist(generator, parameters);
+  }
+
+  double Normaldistribution::operator()(double standard_deviation) {
+    std::normal_distribution<>::param_type parameters(0, standard_deviation);
+    return dist(generator, parameters);
+  }
+
+} // namespace Aux
---
+++ src/Auxillary/Mathfunctions/include/Normaldistribution.hpp
@@ -0,0 +1,20 @@
+#include <chrono>
+#include <pcg_random.hpp>
+#include <random>
+
+namespace Aux {
+
+  pcg64 setup_random_number_generator();
+
+  class Normaldistribution final {
+  public:
+    Normaldistribution();
+
+    double operator()(double mean, double standard_deviation);
+    double operator()(double standard_deviation);
+
+  private:
+    pcg64 generator{setup_random_number_generator()};
+    std::normal_distribution<> dist;
+  };
+} // namespace Aux
