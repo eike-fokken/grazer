d751a8a1e96c3f5bb93d1097d6da125d8a11564b
Eike <e.fokken+git@posteo.de>
2021-04-06 11:59:47 +0200

    make a number of stochastic steps for every overall time step

--- src/Auxillary/Stochastics/Stochastic.cpp
+++ src/Auxillary/Stochastics/Stochastic.cpp
@@ -4,10 +4,16 @@
 namespace Aux {
   double euler_maruyama_step(
       double last_value, double theta, double mu, double delta_t, double sigma,
-      Normaldistribution &distribution) {
-    // Careful: delta_t is the variance, to get the standard deviation we must
-    // take the squareroot!
-    return last_value + theta * (mu - last_value) * delta_t
-           + sigma * distribution(sqrt(delta_t));
+      Normaldistribution &distribution, int number_of_stochastic_steps) {
+    double stochastic_stepsize = delta_t / number_of_stochastic_steps;
+    double current_value = last_value;
+    for (auto i = 0; i != number_of_stochastic_steps; ++i) {
+      current_value
+          = current_value
+                * (1 - theta * mu * current_value * stochastic_stepsize)
+            + theta * mu * stochastic_stepsize
+            + sigma * distribution(sqrt(stochastic_stepsize));
+    }
+    return current_value;
   }
 } // namespace Aux
--- src/Auxillary/Stochastics/include/Stochastic.hpp
+++ src/Auxillary/Stochastics/include/Stochastic.hpp
@@ -4,6 +4,6 @@ namespace Aux {
 
   double euler_maruyama_step(
       double last_value, double theta, double mu, double delta_t, double sigma,
-      Normaldistribution &distribution);
+      Normaldistribution &distribution, int number_of_stochastic_steps);
 
 } // namespace Aux
