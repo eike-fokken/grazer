71975f15147962ac42fa462cc835a472926aa82a
Eike <e.fokken+git@posteo.de>
2021-04-09 18:18:34 +0200

    adapted octave helper to new structure

--- helper_functions/octave_helpers/get_component_values.m
+++ helper_functions/octave_helpers/get_component_values.m
@@ -1,11 +1,8 @@
-function m = get_component_values(directory,subtype,id,variablename)
+function m = get_component_values(filename,variablename)
   pkg load io;
     
-  filename = strcat(subtype,"_",id);
-  full_filename= fullfile(directory,filename);
-
-  filecontents=csv2cell(full_filename,",");
+  filecontents=csv2cell(filename,",");
   newcell=strrep(filecontents(1,:)," ","");
-  [a,voltage_index] = ismember(newcell,variablename);
+  a = ismember(newcell,variablename);
   m = cell2mat(filecontents(2:end,a));
 end
