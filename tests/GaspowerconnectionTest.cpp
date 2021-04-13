c2d9a1edca45db436f567b58be02d8e6c79c9e6a
Eike <e.fokken+git@posteo.de>
2021-04-13 14:49:22 +0200

    tests amended for the new function signatures of the output json.

--- tests/GaspowerconnectionTest.cpp
+++ tests/GaspowerconnectionTest.cpp
@@ -27,8 +27,9 @@ public:
       std::stringstream buffer;
       Catch_cout catcher(buffer.rdbuf());
       Model::Componentfactory::Full_factory factory;
+      nlohmann::json outputjson;
       auto net_ptr = Model::Networkproblem::build_net(
-          netproblem, factory, std::filesystem::current_path());
+          netproblem, factory, std::filesystem::current_path(), outputjson);
       netprob = std::make_unique<Model::Networkproblem::Networkproblem>(
           std::move(net_ptr));
       output = buffer.str();
