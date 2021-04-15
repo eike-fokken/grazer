82428151a774197954d7132b9773f90dacf8c7c8
Eike <e.fokken+git@posteo.de>
2021-04-15 12:00:17 +0200

    Can now iterate over all directories whose names start with a given string.

--- helper_functions/get_mean_sigma.cpp
+++ helper_functions/get_mean_sigma.cpp
@@ -3,23 +3,49 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
+namespace fs = std::filesystem;
+
+// void add_json_data(nlohmann::json const &input, nlohmann::json &output);
+// void add_json_data(nlohmann::json const &input, nlohmann::json &output) {
+
+//   for (auto &componenttype : input) {
+//     for (auto &component : componenttype) {}
+//   }
+// }
+
 int main(int argc, char **argv) {
-  using json = nlohmann::json;
-  std::string input_json_string = argv[1];
-  std::string output_json_string = argv[2];
 
-  auto inputfile = std::filesystem::path(input_json_string);
-  auto outputfile = std::filesystem::path(output_json_string);
+  std::string output_dir_trunk = argv[1];
 
-  json input;
-  {
-    std::ifstream inputstream(inputfile);
-    inputstream >> input;
+  for (auto &pit : fs::directory_iterator(fs::current_path())) {
+    // check only directories:
+    if (not fs::is_directory(pit.path())) {
+      continue;
+    }
+    // ignore directories starting with . (hidden files in unix)
+    if ((pit.path().filename().string()).find(".") == 0) {
+      continue;
+    }
+    // auto current_dirname = pit.path().filename();
+    // std::cout << pit.path().filename() << std::endl;
+
+    if ((pit.path().filename().string()).find(output_dir_trunk) == 0) {
+      std::cout << pit.path().filename() << std::endl;
+    }
   }
 
-  {
-    std::ofstream outputstream(outputfile);
-    outputstream << input;
-  }
-  std::cout << "first part worked" << std::endl;
+  // json input;
+  // {
+  //   std::ifstream inputstream(inputfile);
+  //   inputstream >> input;
+  // }
+
+  // json output;
+
+  // {
+  //   std::ofstream outputstream(outputfile);
+  //   outputstream << input;
+  // }
+  // std::cout << "first part worked" << std::endl;
 }
