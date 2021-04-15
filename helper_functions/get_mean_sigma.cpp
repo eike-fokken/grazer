ff0941c86b1cb47283804be4d4958b9e4dc99611
Eike <e.fokken+git@posteo.de>
2021-04-15 12:15:35 +0200

    wip

--- helper_functions/get_mean_sigma.cpp
+++ helper_functions/get_mean_sigma.cpp
@@ -6,17 +6,33 @@
 using json = nlohmann::json;
 namespace fs = std::filesystem;
 
-// void add_json_data(nlohmann::json const &input, nlohmann::json &output);
-// void add_json_data(nlohmann::json const &input, nlohmann::json &output) {
+void add_json_data(nlohmann::json const &input, nlohmann::json &output);
+void add_json_data(nlohmann::json const &input, nlohmann::json &output) {
 
-//   for (auto &componenttype : input) {
-//     for (auto &component : componenttype) {}
-//   }
-// }
+  for (auto &componenttype : input) {
+    for (auto &component : componenttype) {}
+  }
+}
 
 int main(int argc, char **argv) {
 
-  std::string output_dir_trunk = argv[1];
+  std::string output_dir_trunk;
+  if (argc >= 2) {
+    output_dir_trunk = argv[1];
+  } else {
+    output_dir_trunk = "output";
+  }
+  std::cout << output_dir_trunk << std::endl;
+
+  std::string computed_output;
+  if (argc >= 3) {
+    computed_output = argv[2];
+  } else {
+    computed_output = "mean_and_sigma.json";
+  }
+  std::cout << computed_output << std::endl;
+
+  json output;
 
   for (auto &pit : fs::directory_iterator(fs::current_path())) {
     // check only directories:
@@ -27,21 +43,19 @@ int main(int argc, char **argv) {
     if ((pit.path().filename().string()).find(".") == 0) {
       continue;
     }
-    // auto current_dirname = pit.path().filename();
-    // std::cout << pit.path().filename() << std::endl;
-
     if ((pit.path().filename().string()).find(output_dir_trunk) == 0) {
-      std::cout << pit.path().filename() << std::endl;
+      std::cout << "Processing " << pit.path().filename() << std::endl;
+
+      json input;
+      {
+        fs::path currpath
+            = fs::absolute(pit.path()) / fs::path("output.json").string();
+        std::ifstream inputstream(currpath);
+        inputstream >> input;
+      }
+      add_json_data(input, output);
     }
   }
-
-  // json input;
-  // {
-  //   std::ifstream inputstream(inputfile);
-  //   inputstream >> input;
-  // }
-
-  // json output;
 
   // {
   //   std::ofstream outputstream(outputfile);
