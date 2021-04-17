a720f1e1a2dffd6fed658d60e30b60d3b98a795e
Eike <e.fokken+git@posteo.de>
2021-04-17 15:59:22 +0200

    We now actually compute the mean!

--- helper_functions/get_mean_sigma.cpp
+++ helper_functions/get_mean_sigma.cpp
@@ -9,6 +9,69 @@ namespace fs = std::filesystem;
 void add_json_data(
     nlohmann::json const &input, nlohmann::json &output,
     std::vector<std::string> types);
+void compute_mean(
+    nlohmann::json &output, int number_of_runs, std::vector<std::string> types);
+
+int main(int argc, char **argv) {
+
+  std::string output_dir_trunk;
+  if (argc >= 2) {
+    output_dir_trunk = argv[1];
+  } else {
+    output_dir_trunk = "output";
+  }
+  std::cout << output_dir_trunk << std::endl;
+
+  std::string computed_output;
+  if (argc >= 3) {
+    computed_output = argv[2];
+  } else {
+    computed_output = "mean_and_sigma.json";
+  }
+  std::cout << computed_output << std::endl;
+
+  json output;
+
+  std::vector<std::string> types{
+      "PQnode", "PVnode", "Vphinode", "StochasticPQnode"};
+
+  int number_of_runs = 0;
+  for (auto &pit : fs::directory_iterator(fs::current_path())) {
+    // check only directories:
+    if (not fs::is_directory(pit.path())) {
+      continue;
+    }
+    // ignore directories starting with . (hidden files in unix)
+    if ((pit.path().filename().string()).find(".") == 0) {
+      continue;
+    }
+    if ((pit.path().filename().string()).find(output_dir_trunk) == 0) {
+      std::cout << "Processing " << pit.path().filename() << std::endl;
+
+      json input;
+      {
+        fs::path currpath
+            = fs::absolute(pit.path()) / fs::path("output.json").string();
+        std::ifstream inputstream(currpath);
+        inputstream >> input;
+      }
+      add_json_data(input, output, types);
+      ++number_of_runs;
+    }
+  }
+  compute_mean(output, number_of_runs, types);
+  std::cout << "Number of processed output files: " << number_of_runs
+            << std::endl;
+  std::ofstream outstream(computed_output);
+  outstream << output.dump(1);
+
+  // {
+  //   std::ofstream outputstream(outputfile);
+  //   outputstream << input;
+  // }
+  // std::cout << "first part worked" << std::endl;
+}
+
 void add_json_data(
     nlohmann::json const &input, nlohmann::json &output,
     std::vector<std::string> types) {
@@ -54,58 +117,19 @@ void add_json_data(
   }
 }
 
-int main(int argc, char **argv) {
-
-  std::string output_dir_trunk;
-  if (argc >= 2) {
-    output_dir_trunk = argv[1];
-  } else {
-    output_dir_trunk = "output";
-  }
-  std::cout << output_dir_trunk << std::endl;
-
-  std::string computed_output;
-  if (argc >= 3) {
-    computed_output = argv[2];
-  } else {
-    computed_output = "mean_and_sigma.json";
-  }
-  std::cout << computed_output << std::endl;
-
-  json output;
-
-  std::vector<std::string> types{
-      "PQnode", "PVnode", "Vphinode", "StochasticPQnode"};
-
-  for (auto &pit : fs::directory_iterator(fs::current_path())) {
-    // check only directories:
-    if (not fs::is_directory(pit.path())) {
-      continue;
-    }
-    // ignore directories starting with . (hidden files in unix)
-    if ((pit.path().filename().string()).find(".") == 0) {
-      continue;
-    }
-    if ((pit.path().filename().string()).find(output_dir_trunk) == 0) {
-      std::cout << "Processing " << pit.path().filename() << std::endl;
-
-      json input;
-      {
-        fs::path currpath
-            = fs::absolute(pit.path()) / fs::path("output.json").string();
-        std::ifstream inputstream(currpath);
-        inputstream >> input;
+void compute_mean(
+    nlohmann::json &output, int number_of_runs,
+    std::vector<std::string> types) {
+  for (auto &type : types) {
+    for (auto &component : output[type]) {
+      for (auto &step : component["data"]) {
+        for (auto varit = step.begin(); varit != step.end(); ++varit) {
+          if (varit.key() == "time") {
+            continue;
+          }
+          varit.value() = varit.value().get<double>() / number_of_runs;
+        }
       }
-      add_json_data(input, output, types);
     }
   }
-
-  std::ofstream outstream(computed_output);
-  outstream << output.dump(1);
-
-  // {
-  //   std::ofstream outputstream(outputfile);
-  //   outputstream << input;
-  // }
-  // std::cout << "first part worked" << std::endl;
 }
