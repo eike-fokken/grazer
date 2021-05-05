b3228237cddd068c7afa22694d0bef2c95b51775
Felix Benning <felix.benning@gmail.com>
2021-05-05 18:00:37 +0200

    make initial schema helper

---
+++ helper_functions/create_initial_schema.cpp
@@ -0,0 +1,8 @@
+#include "Componentfactory.hpp"
+#include "Full_factory.hpp"
+#include <iostream>
+
+int main() {
+  Model::Componentfactory::Full_factory full_factory;
+  std::cout << full_factory.get_initial_schema().dump(4) << std::endl;
+}
