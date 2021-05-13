8f6a9cbed0512509843d3c54d5d3e28677f5de8a
Felix Benning <felix.benning@gmail.com>
2021-05-13 18:16:08 +0200

    clang format

--- helper_functions/make_schemas.cpp
+++ helper_functions/make_schemas.cpp
@@ -1,25 +1,22 @@
-#include "Full_factory.hpp"
-#include "schema_generation.hpp"
-#include "Input_output.hpp"
 #include "Exception.hpp"
+#include "Full_factory.hpp"
+#include "Input_output.hpp"
+#include "schema_generation.hpp"
 #include <filesystem>
 
 using std::filesystem::path;
 namespace fs = std::filesystem;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   auto arguments = io::args_as_vector(argc, argv);
 
   path schema_dir;
   switch (arguments.size()) {
-    case 0 :
-      schema_dir = path("schema"); // default
-      break;
-    case 1:
-      schema_dir = path(arguments[1]);
-      break;
-    default:
-      gthrow({"Unexpected number of arguments"});
+  case 0:
+    schema_dir = path("schema"); // default
+    break;
+  case 1: schema_dir = path(arguments[1]); break;
+  default: gthrow({"Unexpected number of arguments"});
   }
   if (not fs::exists(schema_dir)) {
     fs::create_directory(schema_dir);
