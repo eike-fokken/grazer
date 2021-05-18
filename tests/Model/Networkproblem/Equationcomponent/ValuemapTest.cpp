7027ea9f0d0e30d237c9ada3e55211eb688cd81b
Tibor Cornelli <tibor.cornelli@gmail.com>
2021-05-18 23:59:03 +0200

    New branch to check for duplicates in Valuemap

---
+++ tests/Model/Networkproblem/Equationcomponent/ValuemapTest.cpp
@@ -0,0 +1,21 @@
+#include "Exception.hpp"
+#include "Valuemap.hpp"
+#include <Eigen/Dense>
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <stdexcept>
+
+TEST(set_conditionTEST, duplicate_value) {
+
+  // nlohmann::json values_json;
+
+  // values_json
+  //     = {{"id", "N213"},
+  //        {"data",
+  //         {{{"x", 1.0}, {"values", {1,2}}},
+  //          {{"x", 1.0}, {"values", {3,4}}},
+  //          {{"x", 3.0}, {"values", {5,6}}}}}};
+
+  // EXPECT_THROW(Model::Networkproblem::Valuemap<2>::set_condition(
+  //                              values_json, "x"), std::runtime_error);
+}
