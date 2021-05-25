4abf5192561e58d82d6ac376dcf5b046e7e17b9f
Tibor Cornelli <tibor.cornelli@gmail.com>
2021-05-25 23:26:14 +0200

    Valuemap now does not accept duplicate time or x values

--- tests/Model/Networkproblem/Equationcomponent/ValuemapTest.cpp
+++ tests/Model/Networkproblem/Equationcomponent/ValuemapTest.cpp
@@ -5,17 +5,32 @@
 #include <nlohmann/json.hpp>
 #include <stdexcept>
 
-TEST(set_conditionTEST, duplicate_value) {
+TEST(set_conditionTEST, nothin_wrong) {
 
-  // nlohmann::json values_json;
+  nlohmann::json values_json;
 
-  // values_json
-  //     = {{"id", "N213"},
-  //        {"data",
-  //         {{{"x", 1.0}, {"values", {1,2}}},
-  //          {{"x", 1.0}, {"values", {3,4}}},
-  //          {{"x", 3.0}, {"values", {5,6}}}}}};
+  values_json
+      = {{"id", "N213"},
+         {"data",
+          {{{"x", 1.0}, {"values", {1, 2}}},
+           {{"x", 2.0}, {"values", {3, 4}}},
+           {{"x", 3.0}, {"values", {5, 6}}}}}};
 
-  // EXPECT_THROW(Model::Networkproblem::Valuemap<2>::set_condition(
-  //                              values_json, "x"), std::runtime_error);
+  EXPECT_NO_THROW(
+      Model::Networkproblem::Valuemap<2>::set_condition(values_json, "x"));
+}
+
+TEST(set_conditionTEST, duplicate_x_value) {
+
+  nlohmann::json values_json;
+
+  values_json
+      = {{"id", "N213"},
+         {"data",
+          {{{"x", 1.0}, {"values", {1,2}}},
+           {{"x", 1.0}, {"values", {3,4}}},
+           {{"x", 3.0}, {"values", {5,6}}}}}};
+
+  EXPECT_THROW(Model::Networkproblem::Valuemap<2>::set_condition(
+                               values_json, "x"), std::runtime_error);
 }
