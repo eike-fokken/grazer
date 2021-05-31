eacf785d0d07805efa493cdc2328d86ebe1b951d
Felix Benning <felix.benning@gmail.com>
2021-05-31 15:11:41 +0200

    move run command into different file

--- src/Grazer/grazer.cpp
+++ src/Grazer/grazer.cpp
@@ -1,49 +1,11 @@
-#include "Aux_json.hpp"
-#include "Input_output.hpp"
-#include "Problem.hpp"
-#include "Timeevolver.hpp"
 #include "schema_generation.hpp"
 #include "schema_key_insertion.hpp"
-#include <Eigen/Sparse>
-#include <chrono>
 #include <filesystem>
 #include <iostream>
-#include <memory>
-#include <nlohmann/json.hpp>
 #include <string>
 #include <structopt/structopt.hpp>
 
-static int grazer_run(std::filesystem::path directory_path) {
-  auto problem_directory = directory_path / "problem";
-  auto problem_data_file = problem_directory / "problem_data.json";
-  auto output_dir = io::prepare_output_dir(directory_path / "output");
-
-  // This must be wrapped in exception handling code!
-
-  auto all_json = aux_json::get_json_from_file_path(problem_data_file);
-
-  auto time_evolution_json = all_json["time_evolution_data"];
-  auto problem_json = all_json["problem_data"];
-
-  // give the path information of the file:
-  problem_json["GRAZER_file_directory"] = problem_directory.string();
-
-  auto initial_value_json = all_json["initial_values"];
-  initial_value_json["GRAZER_file_directory"] = problem_directory.string();
-  Model::Timedata timedata(time_evolution_json);
-
-  double tolerance = 1e-8;
-  int maximal_number_of_newton_iterations = 50;
-  Model::Timeevolver timeevolver(
-      tolerance, maximal_number_of_newton_iterations, output_dir);
-
-  Model::Problem problem(problem_json, timeevolver.get_output_dir());
-  int number_of_states = problem.set_indices();
-  std::cout << "data read" << std::endl;
-
-  timeevolver.simulate(timedata, problem, number_of_states, initial_value_json);
-  return 0;
-}
+#include "commands.hpp"
 
 struct Grazer {
   struct Run : structopt::sub_command {
@@ -89,7 +51,7 @@ int main(int argc, char **argv) {
       }
     }
   } catch (std::exception const &ex) {
-    std::cout << "[Error]: " << ex.what() << std::endl;
+    std::cout << "\n[Error]: " << ex.what() << std::endl;
   } catch (...) {
     std::cout << "An unkown type of exception was thrown.\n\n"
                  "This is a bug and must be fixed!\n\n"
---
+++ src/Grazer/include/commands.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <filesystem>
+
+int grazer_run(std::filesystem::path directory_path);
