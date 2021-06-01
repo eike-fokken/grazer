802f307466c0f51eec9000f68ffcac0c47ef9db1
Eike <e.fokken+git@posteo.de>
2021-06-01 14:33:56 +0200

    a test for the simplified newton method.
    Would be nice to have a test case where it fails,
    while full newton method succeeds

--- tests/Newtonsolver/NewtonsolverTest.cpp
+++ tests/Newtonsolver/NewtonsolverTest.cpp
@@ -1,6 +1,7 @@
 #include "TestProblem.hpp"
 
 #include <Newtonsolver.hpp>
+#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 #include <iostream>
 
@@ -8,39 +9,6 @@ Eigen::VectorXd f(Eigen::VectorXd x);
 Eigen::VectorXd f2(Eigen::VectorXd x);
 Eigen::SparseMatrix<double> df(Eigen::VectorXd);
 Eigen::SparseMatrix<double> df2(Eigen::VectorXd);
-
-Eigen::VectorXd f(Eigen::VectorXd x) {
-  Eigen::Matrix2d A;
-  A << 2, 1, 0, 3;
-  Eigen::Vector2d b;
-  b << 1, 0;
-  return A * x + b;
-}
-
-Eigen::VectorXd f2(Eigen::VectorXd x) {
-  Eigen::Matrix2d A;
-  A << 1, 1, 0, 1;
-  Eigen::Vector2d b, y;
-  y(0) = x(0) * x(0);
-  y(1) = x(1) * x(1);
-  b << 9, 0;
-
-  return (A * y + b);
-}
-
-Eigen::SparseMatrix<double> df(Eigen::VectorXd) {
-
-  Eigen::Matrix2d A;
-  A << 2, 1, 0, 3;
-  return A.sparseView();
-}
-
-Eigen::SparseMatrix<double> df2(Eigen::VectorXd x) {
-
-  Eigen::Matrix2d A;
-  A << 2 * x[0], 2 * x[1], 0.0, 2 * x[1];
-  return A.sparseView();
-}
 
 TEST(Newtonsolver, LinearSolveWithRoot_InitialValue1) {
   double tol = 1e-12;
@@ -131,6 +99,34 @@ TEST(Newtonsolver, NonlinearSolveWithRoot) {
   EXPECT_EQ(a.used_iterations, max_it);
 }
 
+TEST(Newtonsolver, NonlinearSolve_simplifiedNewton) {
+  double tol = 1e-12;
+  int max_it = 100;
+
+  Solver::Newtonsolver<GrazerTest::TestProblem> Solver(tol, max_it);
+  Eigen::VectorXd new_state(2), last_state(2), solution(2);
+
+  last_state(0) = 0;
+  last_state(1) = 0;
+
+  new_state(0) = 1;
+  new_state(1) = 1;
+
+  solution(0) = 3;
+  solution(1) = 0.;
+
+  double last_time = 0;
+  double new_time = 1;
+
+  GrazerTest::TestProblem problem(f2, df2);
+  Solver::Solutionstruct a;
+  a = Solver.solve(
+      new_state, problem, true, false, last_time, new_time, last_state);
+
+  EXPECT_EQ(a.success, false);
+  EXPECT_EQ(a.used_iterations, max_it);
+}
+
 TEST(Newtonsolver, SingularJacobian) {
   double tol = 1e-12;
   int max_it = 100;
@@ -152,7 +148,46 @@ TEST(Newtonsolver, SingularJacobian) {
   GrazerTest::TestProblem problem(f2, df2);
   Solver::Solutionstruct a;
 
-  EXPECT_ANY_THROW(
-      a = Solver.solve(
-          new_state, problem, true, true, last_time, new_time, last_state););
+  try {
+    a = Solver.solve(
+        new_state, problem, true, true, last_time, new_time, last_state);
+  } catch (std::exception &e) {
+    EXPECT_THAT(
+        e.what(),
+        testing::HasSubstr(
+            "Couldn't decompose a Jacobian, it may be non-invertible."));
+  }
+}
+
+Eigen::VectorXd f(Eigen::VectorXd x) {
+  Eigen::Matrix2d A;
+  A << 2, 1, 0, 3;
+  Eigen::Vector2d b;
+  b << 1, 0;
+  return A * x + b;
+}
+
+Eigen::VectorXd f2(Eigen::VectorXd x) {
+  Eigen::Matrix2d A;
+  A << 1, 1, 0, 1;
+  Eigen::Vector2d b, y;
+  y(0) = x(0) * x(0);
+  y(1) = x(1) * x(1);
+  b << 9, 0;
+
+  return (A * y + b);
+}
+
+Eigen::SparseMatrix<double> df(Eigen::VectorXd) {
+
+  Eigen::Matrix2d A;
+  A << 2, 1, 0, 3;
+  return A.sparseView();
+}
+
+Eigen::SparseMatrix<double> df2(Eigen::VectorXd x) {
+
+  Eigen::Matrix2d A;
+  A << 2 * x[0], 2 * x[1], 0.0, 2 * x[1];
+  return A.sparseView();
 }
