cdf1c17ca085af92472d7072493103dd80f89e75
Tibor Cornelli <tibor.cornelli@gmail.com>
2021-07-12 03:32:17 +0200

    Inserted the copyright text in every .hpp/.cpp file except for randutils.hpp

--- src/Model/Networkproblem/Gas/Gasedge.cpp
+++ src/Model/Networkproblem/Gas/Gasedge.cpp
@@ -1,3 +1,20 @@
+/*
+ * Grazer - network simulation tool
+ *
+ * Copyright 2020-2021 Uni Mannheim <e.fokken+grazer@posteo.de>,
+ *
+ * SPDX-License-Identifier:	AGPL-3.0-or-later
+ *
+ * Licensed under the GNU Affero General Public License v3.0, found in
+ * LICENSE.txt and at https://www.gnu.org/licenses/agpl-3.0.html
+ * This file may not be copied, modified, or distributed except according to
+ * those terms.
+ *
+ * Distributed on an "AS IS" BASIS, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied.  See your chosen license for details.
+ *
+ */
+
 #include "Idobject.hpp"
 #include <Exception.hpp>
 #include <Gasedge.hpp>
--- tests/test_io_helper.cpp
+++ tests/test_io_helper.cpp
@@ -1,3 +1,20 @@
+/*
+ * Grazer - network simulation tool
+ *
+ * Copyright 2020-2021 Uni Mannheim <e.fokken+grazer@posteo.de>,
+ *
+ * SPDX-License-Identifier:	AGPL-3.0-or-later
+ *
+ * Licensed under the GNU Affero General Public License v3.0, found in
+ * LICENSE.txt and at https://www.gnu.org/licenses/agpl-3.0.html
+ * This file may not be copied, modified, or distributed except according to
+ * those terms.
+ *
+ * Distributed on an "AS IS" BASIS, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied.  See your chosen license for details.
+ *
+ */
+
 #include "test_io_helper.hpp"
 #include <iostream>
 #include <pcg_extras.hpp>
