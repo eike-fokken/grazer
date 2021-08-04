7c6e266edb19f013a38f00bfbfd7a8673f92b3da
Felix Benning <felix.benning@gmail.com>
2021-08-04 19:16:46 +0200

    more tests

--- tests/Auxillary/json_schema/schemaCreationTest.cpp
+++ tests/Auxillary/json_schema/schemaCreationTest.cpp
@@ -4,6 +4,7 @@
 #include <make_schema.hpp>
 #include <nlohmann/json.hpp>
 #include <schema_validation.hpp>
+#include <iostream>
 
 using nlohmann::json;
 
@@ -11,7 +12,8 @@ class TestDefaults : public ::testing::Test {
 public:
   const static json defaults_pipe;
   const static json schema_pipe;
-  const static json pipe;
+  const static json pipe_1;
+  const static json pipe_2;
 };
 
 const json TestDefaults::defaults_pipe = R"(
@@ -50,16 +52,35 @@ const json TestDefaults::schema_pipe = R"(
 }
 )"_json;
 
-const json TestDefaults::pipe = R"(
+const json TestDefaults::pipe_1 = R"(
 {
   "meta": "some string"
 }
 )"_json;
 
+const json TestDefaults::pipe_2 = R"(
+{
+  "dimensions": {
+    "length": {
+      "value": 2.1
+    }
+  }
+}
+)"_json;
+
 TEST_F(TestDefaults, happy_path) {
   json schema_pipe_copy = schema_pipe;
-  json pipe_copy = pipe;
+  json pipe_1_copy = pipe_1;
   Aux::schema::add_defaults(schema_pipe_copy, defaults_pipe);
-  Aux::schema::apply_defaults(pipe_copy, schema_pipe_copy);
-  EXPECT_NO_THROW(Aux::schema::validate_json(pipe_copy, schema_pipe_copy););
+  Aux::schema::apply_defaults(pipe_1_copy, schema_pipe_copy);
+  EXPECT_NO_THROW(Aux::schema::validate_json(pipe_1_copy, schema_pipe_copy););
+  EXPECT_EQ(pipe_1_copy["meta"], "some string"); //keeps old values
+
+  schema_pipe_copy = schema_pipe;
+  json pipe_2_copy = pipe_2;
+  Aux::schema::add_defaults(schema_pipe_copy, defaults_pipe);
+  Aux::schema::apply_defaults(pipe_2_copy, schema_pipe_copy);
+  EXPECT_NO_THROW(Aux::schema::validate_json(pipe_2_copy, schema_pipe_copy););
+  EXPECT_EQ(pipe_2_copy["meta"], "test");
+  EXPECT_EQ(pipe_2_copy["dimensions"]["length"]["value"], 2.1);
 }
