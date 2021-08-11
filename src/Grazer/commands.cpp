d86e5b837cda8e2b9d750b5125737799f613e16b
Eike <e.fokken+github@posteo.de>
2021-08-11 12:19:10 +0200

    Readded the teardown timer
    This timer measures the time taken for destruction of the Problem object,
    the time evolver object and some other objects that are smaller.

--- src/Grazer/commands.cpp
+++ src/Grazer/commands.cpp
@@ -8,54 +8,72 @@
 #include <iostream>
 
 int grazer::run(std::filesystem::path directory_path) {
-  auto wall_clock_start = std::chrono::system_clock::now();
-  auto problem_directory = directory_path / "problem";
-  auto problem_data_file = problem_directory / "problem_data.json";
-  auto output_dir = io::prepare_output_dir(directory_path / "output");
+  using Clock = std::chrono::high_resolution_clock;
+  using TimePoint = std::chrono::time_point<Clock>;
 
-  // This must be wrapped in exception handling code!
+  TimePoint wall_clock_start = Clock::now();
+  TimePoint wall_clock_setup_end;
+  TimePoint wall_clock_sim_end;
 
-  auto all_json = aux_json::get_json_from_file_path(problem_data_file);
+  {
+    auto problem_directory = directory_path / "problem";
+    auto problem_data_file = problem_directory / "problem_data.json";
+    auto output_dir = io::prepare_output_dir(directory_path / "output");
 
-  auto time_evolution_json = all_json["time_evolution_data"];
-  auto problem_json = all_json["problem_data"];
+    // This must be wrapped in exception handling code!
 
-  // give the path information of the file:
-  problem_json["GRAZER_file_directory"] = problem_directory.string();
+    auto all_json = aux_json::get_json_from_file_path(problem_data_file);
 
-  auto initial_value_json = all_json["initial_values"];
-  initial_value_json["GRAZER_file_directory"] = problem_directory.string();
-  Model::Timedata timedata(time_evolution_json);
+    auto time_evolution_json = all_json["time_evolution_data"];
+    auto problem_json = all_json["problem_data"];
 
-  auto timeevolver = Model::Timeevolver::make_instance(time_evolution_json);
+    // give the path information of the file:
+    problem_json["GRAZER_file_directory"] = problem_directory.string();
 
-  Model::Problem problem(problem_json, output_dir);
-  int number_of_states = problem.set_indices();
-  std::cout << "data read" << std::endl;
+    auto initial_value_json = all_json["initial_values"];
+    initial_value_json["GRAZER_file_directory"] = problem_directory.string();
+    Model::Timedata timedata(time_evolution_json);
 
-  auto wall_clock_setup_end = std::chrono::system_clock::now();
+    auto timeevolver = Model::Timeevolver::make_instance(time_evolution_json);
 
-  // ---------------- actual simulation ------------------------------ //
-  timeevolver.simulate(timedata, problem, number_of_states, initial_value_json);
-  // ----------------------------------------------------------------- //
+    Model::Problem problem(problem_json, output_dir);
+    int number_of_states = problem.set_indices();
+    std::cout << "data read" << std::endl;
 
-  auto wall_clock_sim_end = std::chrono::system_clock::now();
-  std::chrono::duration<double> sim_duration
-      = wall_clock_sim_end - wall_clock_setup_end;
+    wall_clock_setup_end = Clock::now();
 
-  std::chrono::duration<double> setup_duration
-      = wall_clock_setup_end - wall_clock_start;
+    // ---------------- actual simulation ------------------------------ //
+    timeevolver.simulate(
+        timedata, problem, number_of_states, initial_value_json);
+    // ----------------------------------------------------------------- //
 
-  auto wall_clock_end = std::chrono::system_clock::now();
+    wall_clock_sim_end = Clock::now();
+  }
 
-  std::chrono::duration<double> total_duration
-      = wall_clock_end - wall_clock_start;
+  TimePoint wall_clock_end = Clock::now();
 
-  std::cout << "setup took: " << setup_duration.count() << " seconds"
-            << std::endl;
-  std::cout << "simulation took: " << sim_duration.count() << " seconds"
+  auto duration_in_seconds_with_mus_accuracy
+      = [](Clock::duration dur) -> double {
+    using Mus = std::chrono::microseconds;
+    long msdur = std::chrono::duration_cast<Mus>(dur).count();
+    auto double_msdur = static_cast<double>(msdur);
+    return double_msdur / 1e6;
+  };
+
+  auto setup_duration = duration_in_seconds_with_mus_accuracy(
+      wall_clock_setup_end - wall_clock_start);
+  auto sim_duration = duration_in_seconds_with_mus_accuracy(
+      wall_clock_sim_end - wall_clock_setup_end);
+  auto teardown_duration = duration_in_seconds_with_mus_accuracy(
+      wall_clock_end - wall_clock_sim_end);
+  auto total_duration = duration_in_seconds_with_mus_accuracy(
+      wall_clock_end - wall_clock_start);
+
+  std::cout << "setup took:      " << setup_duration << " seconds" << std::endl;
+  std::cout << "simulation took: " << sim_duration << " seconds" << std::endl;
+  std::cout << "teardown took:   " << teardown_duration << " seconds"
             << std::endl;
   std::cout << "-----------------------------" << std::endl;
-  std::cout << "total: " << total_duration.count() << " seconds" << std::endl;
+  std::cout << "total:           " << total_duration << " seconds" << std::endl;
   return EXIT_SUCCESS;
 }
