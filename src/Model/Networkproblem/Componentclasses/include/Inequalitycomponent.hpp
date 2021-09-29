689ef3d3a8170d245ddd5e7c2e567b7de85958e7
Eike <e.fokken+github@posteo.de>
2021-09-29 11:38:23 +0200

    Added a skeleton for Inequalitycomponents and Costcomponents, wip

---
+++ src/Model/Networkproblem/Componentclasses/include/Inequalitycomponent.hpp
@@ -0,0 +1,53 @@
+#include "Eigen/Sparse"
+
+namespace Aux {
+
+  class Matrixhandler;
+}
+
+namespace Model::Networkproblem {
+  class Inequalitycomponent {
+
+  public:
+    virtual ~Inequalitycomponent(){};
+
+    virtual void evaluate_inequality(
+        Eigen::Ref<Eigen::VectorXd> rootvalues, double last_time,
+        double new_time, Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &last_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+
+    virtual void d_evaluate_inequality_d_new_state(
+        Aux::Matrixhandler *inequality_new_state_jacobian_handler,
+        double last_time, double new_time,
+        Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &last_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+
+    virtual void d_evaluate_inequality_d_old_state(
+        Aux::Matrixhandler *inequality_old_state_jacobian_handler,
+        double last_time, double new_time,
+        Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &last_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+
+    virtual void d_evaluate_inequality_d_new_control(
+        Aux::Matrixhandler *inequality_new_control_jacobian_handler,
+        double last_time, double new_time,
+        Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &last_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+
+    virtual void d_evaluate_inequality_d_old_control(
+        Aux::Matrixhandler *inequality_old_control_jacobian_handler,
+        double last_time, double new_time,
+        Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &last_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+  }
+} // namespace Model::Networkproblem
