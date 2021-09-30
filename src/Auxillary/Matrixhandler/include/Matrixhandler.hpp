981868d85a0ac493e34a44277ff1a3138ceb996b
Eike <e.fokken+github@posteo.de>
2021-09-30 22:51:00 +0200

    first try on new matrix handler

--- src/Auxillary/Matrixhandler/include/Matrixhandler.hpp
+++ src/Auxillary/Matrixhandler/include/Matrixhandler.hpp
@@ -9,42 +9,60 @@ namespace Aux {
   class Matrixhandler {
 
   public:
-    Matrixhandler() = delete;
-    Matrixhandler(Eigen::SparseMatrix<double> *_matrix) : matrix(_matrix){};
+    // Matrixhandler() = delete;
+    // Matrixhandler(Eigen::SparseMatrix<double> *_matrix) : matrix(_matrix){};
 
     virtual ~Matrixhandler(){};
 
-    /// \brief Adds to a coefficient.
-    ///
-    /// @param row The row index of the coefficient.
-    /// @param col The column index of the coefficient.
-    /// @param value The value to be added to the already present coefficient.
+    /** \brief Adds to a coefficient.
+     *
+     * @param row The row index of the coefficient.
+     * @param col The column index of the coefficient.
+     * @param value The value to be added to the already present coefficient.
+     */
     virtual void add_to_coefficient(int row, int col, double value) = 0;
 
-    /// \brief Sets a coefficient. For #Triplethandler this actually behaves
-    /// like #add_to_coefficient.
-    ///
-    /// @param row The row index of the coefficient.
-    /// @param col The column index of the coefficient.
-    /// @param value The value to be inserted.
+    /** \brief Sets a coefficient. For #Triplethandler this actually behaves
+     * like #add_to_coefficient.
+     *
+     * @param row The row index of the coefficient.
+     * @param col The column index of the coefficient.
+     * @param value The value to be inserted.
+     */
     virtual void set_coefficient(int row, int col, double value) = 0;
 
-    /// For #Triplethandler: Builds the matrix from the gathered coefficients
-    /// and then forgets the coefficients.
-    ///
-    /// For #Coeffrefhandler: Does nothing.
+    // protected:
+    //   Eigen::SparseMatrix<double> *matrix;
+  };
+
+  /** \brief
+   *
+   */
+  class Eigen_handler_base : public Matrixhandler {
+  public:
+    /** For Triplethandler: Builds the matrix from the gathered coefficients
+     * and then forgets the coefficients.
+     *
+     * For Coeffrefhandler: Does nothing.
+     */
+
     virtual void set_matrix() = 0;
 
   protected:
+    Eigen_handler_base(Eigen::SparseMatrix<double> *_matrix) :
+        matrix(_matrix){};
+
     Eigen::SparseMatrix<double> *matrix;
   };
 
-  /// \brief The Triplethandler variety gathers the coefficients in triplets and
-  /// later builds the matrix from the triplets.
-  class Triplethandler final : public Matrixhandler {
+  /** \brief The Triplethandler variety gathers the coefficients in triplets
+   * and later builds the matrix from the triplets.
+   */
+  class Triplethandler final : public Eigen_handler_base {
 
   public:
-    using Matrixhandler::Matrixhandler;
+    Triplethandler(Eigen::SparseMatrix<double> *_matrix) :
+        Eigen_handler_base(_matrix){};
     ~Triplethandler() override{};
 
     void add_to_coefficient(int row, int col, double value) final;
@@ -56,18 +74,32 @@ namespace Aux {
     std::vector<Eigen::Triplet<double>> tripletlist;
   };
 
-  /// \brief The Coeffrefhandler variety directly sets the coefficients and
-  /// contains no state.
-  class Coeffrefhandler final : public Matrixhandler {
+  /** \brief The Coeffrefhandler variety directly sets the coefficients and
+   * contains no state.
+   */
+  class Coeffrefhandler final : public Eigen_handler_base {
 
   public:
-    using Matrixhandler::Matrixhandler;
+    Coeffrefhandler(Eigen::SparseMatrix<double> *_matrix) :
+        Eigen_handler_base(_matrix){};
 
     ~Coeffrefhandler() override{};
 
     void add_to_coefficient(int row, int col, double value) final;
     void set_coefficient(int row, int col, double value) final;
     void set_matrix() final;
+  };
+
+  /** \brief The Ipopt_handler variety gathers the coefficients in three vectors
+   * as required by Ipopt.
+   */
+  class Ipopt_handler : public Matrixhandler {
+
+  public:
+    virtual ~Ipopt_handler() {}
+
+    void add_to_coefficient(int row, int col, double value) final;
+    void set_coefficient(int row, int col, double value) final;
   };
 
 } // namespace Aux
