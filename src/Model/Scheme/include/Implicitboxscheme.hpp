08290877448166e869bf879b1a882686f07f5920
Eike <e.fokken+bitbucket@posteo.de>
2021-09-30 16:40:38 +0200

    added derivative to scheme

--- src/Model/Scheme/include/Implicitboxscheme.hpp
+++ src/Model/Scheme/include/Implicitboxscheme.hpp
@@ -20,7 +20,6 @@ namespace Model::Scheme {
         Eigen::Ref<Eigen::Vector2d const> new_right,
         Model::Balancelaw::Pipe_Balancelaw const &bl) const override;
 
-    /// The derivative with respect to \code{.cpp}last_left\endcode
     Eigen::Matrix2d devaluate_point_d_new_left(
         double last_time, double new_time, double Delta_x,
         Eigen::Ref<Eigen::Vector2d const> last_left,
@@ -29,8 +28,23 @@ namespace Model::Scheme {
         Eigen::Ref<Eigen::Vector2d const> new_right,
         Model::Balancelaw::Pipe_Balancelaw const &bl) const override;
 
-    /// \brief The derivative with respect to \code{.cpp}last_right\endcode
     Eigen::Matrix2d devaluate_point_d_new_right(
+        double last_time, double new_time, double Delta_x,
+        Eigen::Ref<Eigen::Vector2d const> last_left,
+        Eigen::Ref<Eigen::Vector2d const> last_right,
+        Eigen::Ref<Eigen::Vector2d const> new_left,
+        Eigen::Ref<Eigen::Vector2d const> new_right,
+        Model::Balancelaw::Pipe_Balancelaw const &bl) const override;
+
+    Eigen::Matrix2d devaluate_point_d_last_left(
+        double last_time, double new_time, double Delta_x,
+        Eigen::Ref<Eigen::Vector2d const> last_left,
+        Eigen::Ref<Eigen::Vector2d const> last_right,
+        Eigen::Ref<Eigen::Vector2d const> new_left,
+        Eigen::Ref<Eigen::Vector2d const> new_right,
+        Model::Balancelaw::Pipe_Balancelaw const &bl) const override;
+
+    Eigen::Matrix2d devaluate_point_d_last_right(
         double last_time, double new_time, double Delta_x,
         Eigen::Ref<Eigen::Vector2d const> last_left,
         Eigen::Ref<Eigen::Vector2d const> last_right,
--- src/Model/Scheme/include/Threepointscheme.hpp
+++ src/Model/Scheme/include/Threepointscheme.hpp
@@ -21,7 +21,7 @@ namespace Model::Scheme {
         Eigen::Ref<Eigen::Vector2d const> new_right,
         Model::Balancelaw::Pipe_Balancelaw const &bl) const = 0;
 
-    /// The derivative with respect to \code{.cpp}last_left\endcode
+    /// The derivative with respect to \code{.cpp}new_left\endcode
     virtual Eigen::Matrix2d devaluate_point_d_new_left(
         double last_time, double new_time, double Delta_x,
         Eigen::Ref<Eigen::Vector2d const> last_left,
@@ -30,8 +30,26 @@ namespace Model::Scheme {
         Eigen::Ref<Eigen::Vector2d const> new_right,
         Model::Balancelaw::Pipe_Balancelaw const &bl) const = 0;
 
-    /// \brief The derivative with respect to \code{.cpp}last_right\endcode
+    /// \brief The derivative with respect to \code{.cpp}new_right\endcode
     virtual Eigen::Matrix2d devaluate_point_d_new_right(
+        double last_time, double new_time, double Delta_x,
+        Eigen::Ref<Eigen::Vector2d const> last_left,
+        Eigen::Ref<Eigen::Vector2d const> last_right,
+        Eigen::Ref<Eigen::Vector2d const> new_left,
+        Eigen::Ref<Eigen::Vector2d const> new_right,
+        Model::Balancelaw::Pipe_Balancelaw const &bl) const = 0;
+
+    /// The derivative with respect to \code{.cpp}last_left\endcode
+    virtual Eigen::Matrix2d devaluate_point_d_last_left(
+        double last_time, double new_time, double Delta_x,
+        Eigen::Ref<Eigen::Vector2d const> last_left,
+        Eigen::Ref<Eigen::Vector2d const> last_right,
+        Eigen::Ref<Eigen::Vector2d const> new_left,
+        Eigen::Ref<Eigen::Vector2d const> new_right,
+        Model::Balancelaw::Pipe_Balancelaw const &bl) const = 0;
+
+    /// \brief The derivative with respect to \code{.cpp}last_right\endcode
+    virtual Eigen::Matrix2d devaluate_point_d_last_right(
         double last_time, double new_time, double Delta_x,
         Eigen::Ref<Eigen::Vector2d const> last_left,
         Eigen::Ref<Eigen::Vector2d const> last_right,
