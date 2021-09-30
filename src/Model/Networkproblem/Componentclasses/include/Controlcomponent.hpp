9fcc47ce21f852ed1a7c1618e86dd7ace83ee81f
Eike <e.fokken+bitbucket@posteo.de>
2021-09-30 14:30:37 +0200

    skeleton of controlcomponent, wip

--- src/Model/Networkproblem/Componentclasses/include/Controlcomponent.hpp
+++ src/Model/Networkproblem/Componentclasses/include/Controlcomponent.hpp
@@ -2,10 +2,77 @@
 #include <Eigen/Dense>
 #include <nlohmann/json.hpp>
 
+namespace Aux {
+  class Matrixhandler;
+}
+
 namespace Model::Networkproblem {
   class Controlcomponent {
   public:
     virtual ~Controlcomponent();
+
+    /** \brief evaluates the model equations into rootvalues.
+     *
+     * @param[out] rootvalues Results of the model equations, when evaluated on
+     * the other parameters.
+     * @param last_time time point of the last time step. Usually important for
+     * PDEs
+     * @param new_time time point of the current time step.
+     * @param last_state value of the state at last time step.
+     * @param new_state value of the state at current time step.
+     */
+    virtual void evaluate(
+        Eigen::Ref<Eigen::VectorXd> rootvalues, double last_time,
+        double new_time, Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &old_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+
+    /** \brief Carries out steps that need to be taken before the Newton method
+     * for a time step can start.
+     *
+     * For most components does nothing.
+     * @param last_time time point of the last time step. Usually important for
+     * PDEs
+     * @param new_time time point of the current time step.
+     * @param last_state value of the state at last time step.
+     * @param new_state value of the state at current time step.
+     */
+
+    virtual void prepare_timestep(
+        double last_time, double new_time,
+        Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &old_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control);
+
+    /** \brief derivative of Equationcomponent::evaluate.
+     *
+     * evaluates the derivative of Equationcomponent::evaluate and hands
+     * the result to jacobianhandler, which will fill the Jacobi matrix.
+     * @param jacobianhandler A helper object, that fills a sparse matrix
+     * in an efficient way.
+     * @param last_time time point of the last time step. Usually important
+     * for PDEs
+     * @param new_time time point of the current time step.
+     * @param last_state value of the state at last time step.
+     * @param new_state value of the state at current time step.
+     */
+    virtual void d_evalutate_d_new_state(
+        Aux::Matrixhandler *jacobianhandler, double last_time, double new_time,
+        Eigen::Ref<Eigen::VectorXd const> const &last_state,
+        Eigen::Ref<Eigen::VectorXd const> const &new_state,
+        Eigen::Ref<Eigen::VectorXd const> const &old_control,
+        Eigen::Ref<Eigen::VectorXd const> const &new_control) const = 0;
+
+    /** \brief Utility for setup of things that cannot be done during
+     * construction.
+     *
+     * is called by Networkproblems reserve_state_indices.
+     * Usually does nothing, but for example gas nodes
+     * claim indices from their attached gas edges.
+     */
+    virtual void setup();
 
     /** \brief Returns number of controls needed by this component per time
      *  step.
