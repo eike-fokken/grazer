099b44bf6faa58363f7fcb811ebf532df4ee24d0
Eike <e.fokken+bitbucket@posteo.de>
2021-10-05 11:43:11 +0200

    wip

--- src/Auxillary/Matrixhandler/Costgradienthandler.cpp
+++ src/Auxillary/Matrixhandler/Costgradienthandler.cpp
@@ -1,11 +1,17 @@
 #include "Costgradienthandler.hpp"
+#include "Exception.hpp"
 
 namespace Aux {
 
   Costgradienthandler::Costgradienthandler(
       Eigen::Ref<Eigen::VectorXd> _gradient) :
       gradient(_gradient) {
-    gradient.setZero();
+
+#ifndef NDEBUG
+    if (!_gradient.isZero()) {
+      gthrow({"Costgradienthandler was given a non-Zero array!"});
+    }
+#endif
   }
 
   void Costgradienthandler::add_to_coefficient(int row, double value) {
--- src/Model/Networkproblem/Componentclasses/include/Inequalitycomponent.hpp
+++ src/Model/Networkproblem/Componentclasses/include/Inequalitycomponent.hpp
@@ -35,41 +35,47 @@ namespace Model::Networkproblem {
         Eigen::Ref<Eigen::VectorXd const> const &last_state,
         Eigen::Ref<Eigen::VectorXd const> const &new_state) const = 0;
 
-    virtual void d_evaluate_inequality_d_old_control(
+    virtual void d_evaluate_inequality_d_last_control(
         Aux::Matrixhandler &inequality_old_control_jacobian_handler,
         double last_time, double new_time,
         Eigen::Ref<Eigen::VectorXd const> const &last_state,
         Eigen::Ref<Eigen::VectorXd const> const &new_state) const = 0;
 
-    /// \brief Returns number of inequalities set by this component.
-    ///
-    /// Often this will be implemented by a function returning a literal
-    /// int like 2.  But it may be only known after construction, therefore this
-    /// is not done in the constructor.
-    ///
-    /// @returns number of inequalities set by this component
+    /** \brief Returns number of inequalities set by this component.
+     *
+     * Often this will be implemented by a function returning a literal
+     * int like 2.  But it may be only known after construction, therefore this
+     * is not done in the constructor.
+     *
+     * @returns number of inequalities set by this component
+     */
     virtual int get_number_of_inequalities() const = 0;
 
-    /// \brief This function sets the indices #start_inequality_index and
-    /// #after_inequality_index.
-    ///
-    /// @param next_free_index The first inequality index that is currently not
-    /// claimed by another component.
-    /// @returns The new lowest free index.
+    /** \brief This function sets the indices #start_inequality_index and
+     * #after_inequality_index.
+     *
+     * @param next_free_index The first inequality index that is currently not
+     * claimed by another component.
+     * @returns The new lowest free index.
+     */
     int set_inequality_indices(int const next_free_index);
 
-    /// \brief getter for #start_inequality_index
+    /** \brief getter for #start_inequality_index
+     */
     int get_start_inequality_index() const;
 
-    /// \brief getter for #after_inequality_index
+    /** \brief getter for #after_inequality_index
+     */
     int get_after_inequality_index() const;
 
   private:
-    /// \brief The first index, this Inequalitycomponent "owns".
+    /** \brief The first index, this Inequalitycomponent "owns".
+     */
     int start_inequality_index{-1};
 
-    /// \brief The first index after #start_inequality_index, that is not
-    /// "owned" by this Inequalitycomponent.
+    /** \brief The first index after #start_inequality_index, that is not
+     * "owned" by this Inequalitycomponent.
+     */
     int after_inequality_index{-1};
   };
 } // namespace Model::Networkproblem
