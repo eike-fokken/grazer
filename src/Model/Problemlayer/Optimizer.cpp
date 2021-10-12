6341ed04e2eaadfa8407d271758b1bebda59a63b
Eike <e.fokken+bitbucket@posteo.de>
2021-10-12 14:24:30 +0200

    Optimizer, wip

--- src/Model/Problemlayer/Optimizer.cpp
+++ src/Model/Problemlayer/Optimizer.cpp
@@ -1,5 +1,7 @@
 #include "Optimizer.hpp"
+#include "Networkproblem.hpp"
 #include "Timeevolver.hpp"
+#include "Wrapper.hpp"
 #include "make_schema.hpp"
 #include "schema_validation.hpp"
 
@@ -22,4 +24,39 @@ namespace Model {
     Aux::schema::validate_json(optimization_json, get_schema());
     return Optimizer(optimization_json, time_evolution_json);
   }
+
+  void Optimizer::optimize(
+      Timedata timedata, Networkproblem::Networkproblem &problem,
+      nlohmann::json &problem_initial_state_json,
+      nlohmann::json &problem_initial_control_json) {
+    double last_time = timedata.get_starttime();
+
+    auto number_of_states = problem.get_number_of_states();
+    auto number_of_controls_per_time_point
+        = problem.get_number_of_controls_per_timepoint();
+    auto number_of_inequalities_per_time_point
+        = problem.get_number_of_inequalities_per_timepoint();
+
+    Eigen::VectorXd control(
+        number_of_controls_per_time_point * timedata.get_number_of_steps());
+    Eigen::VectorXd lower_bounds(
+        number_of_controls_per_time_point * timedata.get_number_of_steps());
+    Eigen::VectorXd upper_bounds(
+        number_of_controls_per_time_point * timedata.get_number_of_steps());
+
+    Eigen::VectorXd inequalities(
+        number_of_inequalities_per_time_point * timedata.get_number_of_steps());
+    Eigen::VectorXd inequalities_lower_bounds(
+        number_of_inequalities_per_time_point * timedata.get_number_of_steps());
+    Eigen::VectorXd inequalities_upper_bounds(
+        number_of_inequalities_per_time_point * timedata.get_number_of_steps());
+
+    // problem.set_initial_values(last_state, problem_initial_state_json);
+    // problem.json_save(last_time, last_state);
+
+    for (int i = 0; i != timedata.get_number_of_steps(); ++i) {
+      double new_time = last_time + timedata.get_delta_t();
+    }
+  }
+
 } // namespace Model
--- src/Model/Problemlayer/include/Optimizer.hpp
+++ src/Model/Problemlayer/include/Optimizer.hpp
@@ -12,11 +12,10 @@ namespace Model {
         nlohmann::json const &optimization_json,
         nlohmann::json const &time_evolution_json);
 
-    void optimize();
-
-    void simulate(
+    void optimize(
         Timedata timedata, Networkproblem::Networkproblem &problem,
-        nlohmann::json &problem_initial_json);
+        nlohmann::json &problem_initial_state_json,
+        nlohmann::json &problem_initial_control_json);
 
   private:
     Optimizer(
