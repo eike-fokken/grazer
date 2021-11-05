46edac619bb97aa9d83afdb252491d539df9981c
jhelgert <jonathan.helgert@posteo.de>
2021-11-05 17:20:46 +0100

    add tests for safe addition

--- tests/Auxillary/Misc/AuxMiscTest.cpp
+++ tests/Auxillary/Misc/AuxMiscTest.cpp
@@ -2,7 +2,7 @@
 #include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
-TEST_OVERFLOW() {
+TEST(aux_misc_overflow, aux_misc_overflow) {
   int a = 2147483647; // 2**31 - 1
   int b = 0;
 
@@ -11,7 +11,7 @@ TEST_OVERFLOW() {
 
   // Test that overflow occurs and exception is thrown
   try {
-    int c = Aux::safe_addition(a, b + 1);
+    [[maybe_unused]] int c = Aux::safe_addition(a, b + 1);
   } catch (std::runtime_error &e) {
     EXPECT_THAT(
         e.what(), testing::HasSubstr("The addition would cause an overflow."));
