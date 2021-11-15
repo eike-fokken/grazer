279cd932037cafa5a670510853e99835fdf660ba
Eike <e.fokken+github@posteo.de>
2021-11-15 11:06:00 +0100

    ConstraintJacobianTest now has some tests.
    Still need the actual data accessing functions.

--- tests/IpoptWrapper/ConstrainedJacobianTest.cpp
+++ tests/IpoptWrapper/ConstrainedJacobianTest.cpp
@@ -3,11 +3,12 @@
 #include <Eigen/src/Core/util/Meta.h>
 #include <cstddef>
 #include <gtest/gtest.h>
+#include <sys/stat.h>
 
 TEST(ConstraintJacobian, construction) {
 
-  std::vector<double> constraints_times{0.0, 1.0};
-  std::vector<double> controls_times{0, 1};
+  std::vector<double> constraints_times{0.0, 0.5, 1.0, 2.0};
+  std::vector<double> controls_times{0, 1, 2};
 
   constexpr Eigen::Index constraints_inner_length = 2;
   constexpr Eigen::Index controls_inner_length = 3;
@@ -18,25 +19,14 @@ TEST(ConstraintJacobian, construction) {
   auto test
       = Optimization::ConstraintJacobian::make_instance(constraints, controls);
 
-  // This must go into a constructor
-  double *values = new double[static_cast<std::size_t>(test.nonZeros())];
+  EXPECT_EQ(test.cols(), controls.get_total_number_of_values());
+  EXPECT_EQ(test.rows(), constraints.get_total_number_of_values());
 
-  for (Eigen::Index i = 0; i != test.rows(); ++i) {
-    std::cout << test.size_of_row(i) << std::endl;
-  }
-
-  std::cout << "non-zeros: " << test.nonZeros() << std::endl;
-
-  std::cout << "start_of_rows:" << std::endl;
-  auto a = test.get_start_of_rows();
+  auto start_of_rows = test.get_start_of_rows();
   Eigen::Index curr = 0;
-  std::cout << a[0] << " ";
-  for (Eigen::Index i = 0; i != a.size() - 1; ++i) {
+  EXPECT_EQ(start_of_rows[0], 0);
+  for (Eigen::Index i = 0; i != start_of_rows.size() - 1; ++i) {
     curr += test.size_of_row(i);
-    std::cout << a[i + 1] - curr << " ";
+    EXPECT_EQ(start_of_rows[i + 1], curr);
   }
-  std::cout << std::endl;
-
-  // This must go into a destructor:
-  delete[] values;
 }
