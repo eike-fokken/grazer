87bd5c1d4b50e2be37e0e1b8b0aaaf45a39c6841
Eike <e.fokken+bitbucket@posteo.de>
2021-12-20 15:51:34 +0100

    Removed stuff for compiling. Optimization_helpers must be cleaned up

--- src/Optimization_helpers/Optimization_helpers.cpp
+++ src/Optimization_helpers/Optimization_helpers.cpp
@@ -118,73 +118,77 @@ namespace Optimization {
     }
   }
 
-  Eigen::SparseMatrix<double> compute_control_conversion(
-      Aux::InterpolatingVector_Base const &fine_resolution_vector,
-      Aux::InterpolatingVector_Base const &coarse_resolution_vector) {
+  // Eigen::SparseMatrix<double> compute_control_conversion(
+  //     Aux::InterpolatingVector_Base const &fine_resolution_vector,
+  //     Aux::InterpolatingVector_Base const &coarse_resolution_vector) {
 
-    auto const &fine_times = fine_resolution_vector.get_interpolation_points();
-    auto const &coarse_times
-        = coarse_resolution_vector.get_interpolation_points();
-    if (fine_resolution_vector.get_inner_length()
-        != coarse_resolution_vector.get_inner_length()) {
-      gthrow(
-          {"The function ", __func__,
-           " can only be called on InterpolatingVectors with equal sizes of "
-           "vectors at each point."});
-    }
-    if (fine_times.front() < coarse_times.front()) {
-      gthrow(
-          {"Some interpolation points of the finer resolved "
-           "InterpolatingVector are smaller than the smallest interpolation "
-           "point of the coarse vector."});
-    }
-    if (fine_times.back() > coarse_times.back()) {
-      gthrow(
-          {"Some interpolation points of the finer resolved "
-           "InterpolatingVector are greater than the greatest interpolation "
-           "point of the coarse vector."});
-    }
+  //   auto const &fine_times =
+  //   fine_resolution_vector.get_interpolation_points(); auto const
+  //   &coarse_times
+  //       = coarse_resolution_vector.get_interpolation_points();
+  //   if (fine_resolution_vector.get_inner_length()
+  //       != coarse_resolution_vector.get_inner_length()) {
+  //     gthrow(
+  //         {"The function ", __func__,
+  //          " can only be called on InterpolatingVectors with equal sizes of "
+  //          "vectors at each point."});
+  //   }
+  //   if (fine_times.front() < coarse_times.front()) {
+  //     gthrow(
+  //         {"Some interpolation points of the finer resolved "
+  //          "InterpolatingVector are smaller than the smallest interpolation "
+  //          "point of the coarse vector."});
+  //   }
+  //   if (fine_times.back() > coarse_times.back()) {
+  //     gthrow(
+  //         {"Some interpolation points of the finer resolved "
+  //          "InterpolatingVector are greater than the greatest interpolation "
+  //          "point of the coarse vector."});
+  //   }
 
-    Eigen::SparseMatrix<double> derivative(
-        fine_resolution_vector.get_total_number_of_values(),
-        coarse_resolution_vector.get_total_number_of_values());
+  //   Eigen::SparseMatrix<double> derivative(
+  //       fine_resolution_vector.get_total_number_of_values(),
+  //       coarse_resolution_vector.get_total_number_of_values());
 
-    std::vector<Eigen::Triplet<double, Eigen::Index>> triplets;
+  //   std::vector<Eigen::Triplet<double, Eigen::Index>> triplets;
 
-    Eigen::Index inner_matrix_size = fine_resolution_vector.get_inner_length();
-    for (size_t i = 0; i != fine_times.size(); ++i) {
+  //   Eigen::Index inner_matrix_size =
+  //   fine_resolution_vector.get_inner_length(); for (size_t i = 0; i !=
+  //   fine_times.size(); ++i) {
 
-      auto t_j_interator = std::lower_bound(
-          coarse_times.begin(), coarse_times.end(), fine_times[i]);
-      auto j = t_j_interator - coarse_times.begin();
+  //     auto t_j_interator = std::lower_bound(
+  //         coarse_times.begin(), coarse_times.end(), fine_times[i]);
+  //     auto j = t_j_interator - coarse_times.begin();
 
-      if (j == 0) {
-        // This can only happen on the first of the fine time points, therefore
-        // row and column index are 0 and lambda = 0.
-        add_homotethy_triplets_at_index(triplets, 1.0, 0, 0, inner_matrix_size);
-      } else {
-        auto t = fine_times[i];
-        auto t_jm1 = *std::prev(t_j_interator);
-        auto t_j = *t_j_interator;
-        auto lambda = (t - t_jm1) / (t_j - t_jm1);
-        if (lambda != 1) {
-          add_homotethy_triplets_at_index(
-              triplets, 1 - lambda,
-              static_cast<Eigen::Index>(i) * inner_matrix_size,
-              static_cast<Eigen::Index>(j - 1) * inner_matrix_size,
-              inner_matrix_size);
-        }
-        if (lambda != 0) {
-          add_homotethy_triplets_at_index(
-              triplets, lambda,
-              static_cast<Eigen::Index>(i) * inner_matrix_size,
-              static_cast<Eigen::Index>(j) * inner_matrix_size,
-              inner_matrix_size);
-        }
-      }
-    }
-    derivative.setFromTriplets(triplets.begin(), triplets.end());
-    return derivative;
-  }
+  //     if (j == 0) {
+  //       // This can only happen on the first of the fine time points,
+  //       therefore
+  //       // row and column index are 0 and lambda = 0.
+  //       add_homotethy_triplets_at_index(triplets, 1.0, 0, 0,
+  //       inner_matrix_size);
+  //     } else {
+  //       auto t = fine_times[i];
+  //       auto t_jm1 = *std::prev(t_j_interator);
+  //       auto t_j = *t_j_interator;
+  //       auto lambda = (t - t_jm1) / (t_j - t_jm1);
+  //       if (lambda != 1) {
+  //         add_homotethy_triplets_at_index(
+  //             triplets, 1 - lambda,
+  //             static_cast<Eigen::Index>(i) * inner_matrix_size,
+  //             static_cast<Eigen::Index>(j - 1) * inner_matrix_size,
+  //             inner_matrix_size);
+  //       }
+  //       if (lambda != 0) {
+  //         add_homotethy_triplets_at_index(
+  //             triplets, lambda,
+  //             static_cast<Eigen::Index>(i) * inner_matrix_size,
+  //             static_cast<Eigen::Index>(j) * inner_matrix_size,
+  //             inner_matrix_size);
+  //       }
+  //     }
+  //   }
+  //   derivative.setFromTriplets(triplets.begin(), triplets.end());
+  //   return derivative;
+  // }
 
 } // namespace Optimization
