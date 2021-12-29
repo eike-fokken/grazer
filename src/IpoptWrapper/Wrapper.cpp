0911f1a36b3d9c0bb487461476b89459b4d15e87
Eike <e.fokken+bitbucket@posteo.de>
2021-12-29 22:08:23 +0100

    Wrapper, wip

--- src/IpoptWrapper/Wrapper.cpp
+++ src/IpoptWrapper/Wrapper.cpp
@@ -430,20 +430,21 @@ namespace Optimization {
       constraint_jacobian.setZero();
       // TODO: should be preallocated:
       Eigen::VectorXd xi(states_per_step());
-      Eigen::VectorXd ATxi(states_per_step());
+      Eigen::VectorXd ATxi = Eigen::VectorXd::Zero(states_per_step());
+      Eigen::VectorXd rhs(states_per_step());
       Eigen::VectorXd df_dui(controls_per_step());
       Eigen::Index state_index = state_timepoints.size() - 1;
       update_equation_derivative_matrices(state_index, states, controls);
       update_cost_derivative_matrices(state_index, states, controls);
 
       // solve B^T xi = df/dx^T
-      // The - is introduced later to avoid a temporary, but I'm not sure,
-      // whether this is neccessary.
-      xi = solver.solve(df_dnew_transposed);
-      xi = -xi;
+      rhs = -ATxi;
+      rhs -= df_dnew_transposed;
+      xi = solver.solve(rhs);
       ATxi = dE_dlast_transposed * xi;
       df_dui = xi.transpose() * dE_dcontrol + df_dcontrol;
 
+      // Compute the actual derivative with respect to the control:
       auto lambda = index_lambda_pairs[state_index].second;
       auto upper_index = index_lambda_pairs[state_index].first;
       if (lambda == 1.0) {
