fa128adf408c2544316b45be199a207def2e0f24
Eike <e.fokken+bitbucket@posteo.de>
2022-01-14 11:27:43 +0100

    print level of IPOPT was set to the default again, 5.
    That way we are told of iterations.

--- src/IpoptWrapper/Adaptor.cpp
+++ src/IpoptWrapper/Adaptor.cpp
@@ -14,7 +14,7 @@ namespace Optimization {
     // Approximate the hessian
     _app->Options()->SetStringValue("hessian_approximation", "limited-memory");
     // disable Ipopt's console output
-    _app->Options()->SetIntegerValue("print_level", 4);
+    _app->Options()->SetIntegerValue("print_level", 5);
     // Supress Ipopt Banner
     _app->Options()->SetBoolValue("sb", true);
     // _app->Options()->SetStringValue("derivative_test", "first-order");
