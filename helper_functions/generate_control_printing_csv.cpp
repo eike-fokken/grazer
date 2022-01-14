1faa0bf9e650a5309214d76cd1ec8e3190917fc4
Eike <e.fokken+github@posteo.de>
2022-01-14 20:36:19 +0100

    added a helper to construct a csv for printing from the control.json output file.

--- helper_functions/generate_control_printing_csv.cpp
+++ helper_functions/generate_control_printing_csv.cpp
@@ -1,11 +1,13 @@
 #include "Input_output.hpp"
 #include <algorithm>
+#include <cstddef>
 #include <exception>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <stdexcept>
+#include <string>
 
 using json = nlohmann::json;
 namespace fs = std::filesystem;
@@ -41,7 +43,6 @@ int main(int argc, char **argv) {
       = [](nlohmann::json const &a, nlohmann::json const &b) -> bool {
     return a["id"].get<std::string>() < b["id"].get<std::string>();
   };
-  bool object_is_found = false;
   json object;
   std::vector componentclass{"nodes", "connections"};
   for (auto const &compclass : componentclass) {
@@ -57,17 +58,16 @@ int main(int argc, char **argv) {
           continue;
         } else {
           object = (*it);
-          auto control_type = std::find(
+          auto type_iterator = std::find(
               controltypes.begin(), controltypes.end(), component_type);
 
-          if (control_type != controltypes.end()) {
-            object_is_found = true;
-          } else {
+          if (type_iterator == controltypes.end()) {
             std::cout
                 << __FILE__ << ":" << __LINE__ << ":\n"
-                << "The required id is not a gas type and "
-                   "not a power type.\n"
-                << "Probably Grazer incorporates more types and this "
+                << "The required object id does not belong to an object of "
+                   "Controlcomponent type.\n"
+                << "Probably Grazer incorporates now more Controlcomponent "
+                   "types and this "
                    "helper function should be fixed to incorporate the new "
                    "types\n.  Aborting now...";
             return 1;
@@ -83,8 +83,46 @@ int main(int argc, char **argv) {
   }
 
   try {
-    // here write the csv!
-    static_assert(false, "hier weiter!");
+    fs::path outpath(id + ".csv");
+    if (not io::absolute_file_path_in_root(fs::current_path(), outpath)) {
+      throw std::invalid_argument(
+        "the path " + outpath.string()
+        + " is not in the current working directory!\nProbably the id is wrong.");
+    }
+
+    std::cout << "Creating or overwriting " << outpath.string() << std::endl;
+    std::ofstream outstream(outpath);
+
+    outstream << "time";
+    if (not object["data"][0]["values"].is_array()) {
+      std::cout << "values is not an array, something is wrong with the "
+                   "control json. Aborting..."
+                << std::endl;
+      fs::remove(outpath);
+      return 1;
+    }
+    for (size_t i = 0; i != object["data"][0]["values"].size(); ++i) {
+      outstream << ",   "
+                << "control_" << std::to_string(i);
+    }
+    outstream << "\n";
+
+    for (auto &timestep : object["data"]) {
+      outstream << timestep["time"];
+      for (auto const &value : timestep["values"]) {
+        if (not value.is_number()) {
+          std::cout << std::endl;
+          fs::remove(outpath);
+          std::cout << "a value inside the values vector is not a number, "
+                       "something is wrong with the "
+                       "control json. Aborting..."
+                    << std::endl;
+          return 1;
+        }
+        outstream << ",   " << value;
+      }
+      outstream << "\n";
+    }
 
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
