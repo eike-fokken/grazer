c0f9350a7ada0a816dc6d01d7a7474166bc5bb91
eike-fokken <33839210+eike-fokken@users.noreply.github.com>
2022-06-23 14:19:39 +0200

    Switched to MIT license (#313)
    * Switched to MIT license
    
    In order to make participation of users in industry more probable
    we decided to switch to the MIT license.
    While technically industry could also use AGPL licensed software
    this is often forbidden by company policy to minimize the risk of
    litigation.

--- copyright_header.cpp
+++ copyright_header.cpp
@@ -14,16 +14,3 @@
  * express or implied.  See your chosen license for details.
  *
  */
-#pragma once
-#include <nlohmann/json.hpp>
-
-namespace Model {
-
-  class Boundaryvaluecomponent {
-  protected:
-    Boundaryvaluecomponent(){};
-
-  public:
-    static nlohmann::json get_boundary_schema() = delete;
-  };
-} // namespace Model
