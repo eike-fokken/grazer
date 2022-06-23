6704644c7e135d8f66f6e1aa89c6df9fb7d146ba
Eike Fokken <e.fokken+github@posteo.de>
2022-06-23 13:51:41 +0200

    Switched to MIT license
    In order to make participation of users in industry more probable
    we decided to switch to the MIT license.
    While technically industry could also use AGPL licensed software
    this is often forbidden by company policy to minimize the risk of
    litigation.

--- src/Model/Networkproblem/Gas/include/Source.hpp
+++ src/Model/Networkproblem/Gas/include/Source.hpp
@@ -1,3 +1,19 @@
+/*
+ * Grazer - network simulation and optimization tool
+ *
+ * Copyright 2020-2022 Uni Mannheim <e.fokken+grazer@posteo.de>,
+ *
+ * SPDX-License-Identifier:	MIT
+ *
+ * Licensed under the MIT License, found in the file LICENSE and at
+ * https://opensource.org/licenses/MIT
+ * This file may not be copied, modified, or distributed except according to
+ * those terms.
+ *
+ * Distributed on an "AS IS" BASIS, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied.  See your chosen license for details.
+ *
+ */
 #pragma once
 #include "Flowboundarynode.hpp"
 
