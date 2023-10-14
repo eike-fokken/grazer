d62466019de9a7ffabee35cf4337d3263836d3c0
Eike Fokken <e.fokken+github@posteo.de>
2023-10-14 23:50:32 +0200

    Added bounds checking on the back, back_index and front functions.

--- src/Auxillary/Misc/include/Misc.hpp
+++ src/Auxillary/Misc/include/Misc.hpp
@@ -15,9 +15,9 @@
  *
  */
 #pragma once
+#include "Exception.hpp"
 #include <Eigen/Dense>
 #include <sstream>
-
 namespace Aux {
   /// A version of std::to_string with more precision
   std::string to_string_precise(double value, int n = 9);
@@ -28,44 +28,74 @@ namespace Aux {
 
 template <typename T>
 inline T back(Eigen::Ref<Eigen::VectorX<T> const> const &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'back' on an empty vector!"});
+  }
   return vector[vector.size() - 1];
 }
 
 template <typename T> inline T back(Eigen::VectorX<T> const &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'back' on an empty vector!"});
+  }
   return vector[vector.size() - 1];
 }
 
 template <typename T> inline T &back(Eigen::Ref<Eigen::VectorX<T>> &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'back' on an empty vector!"});
+  }
   return vector[vector.size() - 1];
 }
 
 template <typename T> inline T &back(Eigen::VectorX<T> &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'back' on an empty vector!"});
+  }
   return vector[vector.size() - 1];
 }
 
 template <typename T>
 inline Eigen::Index
 back_index(Eigen::Ref<Eigen::VectorX<T> const> const &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'back_index' on an empty vector!"});
+  }
   return vector.size() - 1;
 }
 template <typename T>
 inline Eigen::Index back_index(Eigen::VectorX<T> const &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'back_index' on an empty vector!"});
+  }
   return vector.size() - 1;
 }
 
 template <typename T>
 inline T front(Eigen::Ref<Eigen::VectorX<T> const> const &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'front' on an empty vector!"});
+  }
   return vector[0];
 }
 
 template <typename T> inline T front(Eigen::VectorX<T> const &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'front' on an empty vector!"});
+  }
   return vector[0];
 }
 
 template <typename T> inline T &front(Eigen::Ref<Eigen::VectorX<T>> &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'front' on an empty vector!"});
+  }
   return vector[0];
 }
 
 template <typename T> inline T &front(Eigen::VectorX<T> &vector) {
+  if (vector.size() == 0) {
+    gthrow({"You cannot call 'front' on an empty vector!"});
+  }
   return vector[0];
 }
